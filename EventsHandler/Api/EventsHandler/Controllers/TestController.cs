c89ee3e171d13a709a575e7c7319275b52a267ec
Thomas M. Krystyan <neo.dark.lord@gmail.com>
2024-03-08 16:38:44 +0100

    Documentation and refactor to extract common logic into a private method

--- EventsHandler/Api/EventsHandler/Controllers/TestController.cs
+++ EventsHandler/Api/EventsHandler/Controllers/TestController.cs
@@ -3,6 +3,7 @@
 using Asp.Versioning;
 using EventsHandler.Attributes.Authorization;
 using EventsHandler.Attributes.Validation;
+using EventsHandler.Behaviors.Communication.Enums;
 using EventsHandler.Behaviors.Mapping.Models.POCOs.NotificatieApi;
 using EventsHandler.Behaviors.Responding.Messages.Models.Informations;
 using EventsHandler.Configuration;
@@ -59,7 +60,12 @@ namespace EventsHandler.Controllers
         /// Ensure that base URL and API key for NotifyNL API service are provided in the configuration.
         /// </summary>
         /// <param name="emailAddress">The email address (required) where the notification should be sent.</param>
-        /// <param name="personalization">The map (optional) of keys and values to be used as email personalization.
+        /// <param name="emailTemplateId">The email template ID (optional) to be used from NotifyNL API service.
+        ///   <para>
+        ///     If empty the ID of a very first looked up email template will be used.
+        ///   </para>
+        /// </param>
+        /// <param name="personalization">The map (optional) of keys and values to be used as message personalization.
         ///   <para>
         ///     Example of personalization in template from NotifyNL Admin Portal: "This is ((placeholderText)) information".
         ///   </para>
@@ -70,12 +76,6 @@ namespace EventsHandler.Controllers
         ///     Resulting message would be: "This is good information" (or exception, if personalization is required but not provided).
         ///   </para>
         /// </param>
-        /// <param name="emailTemplateId">The email template ID (optional) to be used from NotifyNL API service.
-        ///   <para>
-        ///     If empty the ID of a very first looked up email template will be used.
-        ///   </para>
-        /// </param>
-        /// <returns></returns>
         [HttpPost]
         [Route("Notify/SendEmail")]
         // Security
@@ -88,33 +88,111 @@ namespace EventsHandler.Controllers
         [ProducesResponseType(StatusCodes.Status401Unauthorized,        Type = typeof(string))]                       // REASON: JWT Token is invalid or expired
         [ProducesResponseType(StatusCodes.Status403Forbidden,           Type = typeof(ProcessingFailed.Simplified))]  // REASON: Base URL or API key to NotifyNL API service were incorrect
         [ProducesResponseType(StatusCodes.Status400BadRequest,          Type = typeof(ProcessingFailed.Simplified))]  // REASON: Issues on the NotifyNL API service side
-        [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(ProcessingFailed.Simplified))]  // REASON: Unexpected internal error, not handled by API logic
+        [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(ProcessingFailed.Simplified))]  // REASON: Unexpected internal error, not yet handled by Notify OMC logic
         public async Task<IActionResult> SendEmailAsync(
             [Required, FromQuery] string emailAddress,
             [Optional, FromQuery] string? emailTemplateId,
             [Optional, FromBody] Dictionary<string, object> personalization)
         {
+            return await SendAsync(
+                NotifyMethods.Email,
+                emailAddress,
+                emailTemplateId,
+                personalization);
+        }
+
+        /// <summary>
+        /// Sending SMS text messages to the NotifyNL API service. 
+        /// Ensure that base URL and API key for NotifyNL API service are provided in the configuration.
+        /// </summary>
+        /// <param name="mobileNumber">The mobile phone number (required) where the notification should be sent.</param>
+        /// <param name="smsTemplateId">The SMS template ID (optional) to be used from NotifyNL API service.
+        ///   <para>
+        ///     If empty the ID of a very first looked up SMS template will be used.
+        ///   </para>
+        /// </param>
+        /// <param name="personalization">
+        ///   <inheritdoc cref="SendEmailAsync" path="/param[@name='personalization']"/>
+        /// </param>
+        [HttpPost]
+        [Route("Notify/SendSms")]
+        // Security
+        [ApiAuthorization]
+        // User experience
+        [StandardizeApiResponses]  // NOTE: Replace errors raised by ASP.NET Core with standardized API responses
+        // Swagger UI
+        [SwaggerRequestExample(typeof(Dictionary<string, object>), typeof(PersonalizationExample))]
+        [ProducesResponseType(StatusCodes.Status202Accepted)]                                                         // REASON: The notification successfully sent to NotifyNL API service
+        [ProducesResponseType(StatusCodes.Status401Unauthorized,        Type = typeof(string))]                       // REASON: JWT Token is invalid or expired
+        [ProducesResponseType(StatusCodes.Status403Forbidden,           Type = typeof(ProcessingFailed.Simplified))]  // REASON: Base URL or API key to NotifyNL API service were incorrect
+        [ProducesResponseType(StatusCodes.Status400BadRequest,          Type = typeof(ProcessingFailed.Simplified))]  // REASON: Issues on the NotifyNL API service side
+        [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(ProcessingFailed.Simplified))]  // REASON: Unexpected internal error, not yet handled by Notify OMC logic
+        public async Task<IActionResult> SendSmsAsync(
+            [Required, FromQuery] string mobileNumber,
+            [Optional, FromQuery] string? smsTemplateId,
+            [Optional, FromBody] Dictionary<string, object> personalization)
+        {
+            return await SendAsync(
+            NotifyMethods.Sms,
+                mobileNumber,
+                smsTemplateId,
+                personalization);
+        }
+
+        #region Helper methods
+        private static readonly Dictionary<NotifyMethods, string> s_templateTypes = new()
+        {
+            { NotifyMethods.Sms,   "sms"   },
+            { NotifyMethods.Email, "email" }
+        };
+
+        /// <summary>
+        /// Generic method sending notification through <see cref="NotificationClient"/> and handling its responses in a standardized way.
+        /// </summary>
+        /// <returns>
+        ///   The standardized <see cref="ObjectResult"/> API response.
+        /// </returns>
+        private async Task<IActionResult> SendAsync(
+            NotifyMethods notifyMethod,
+            string contactDetails,
+            string? templateId,
+            Dictionary<string, object> personalization)
+        {
             // TODO: This logic should be extracted into dedicated services (especially the logging errors part, as an extension of existing IRespondingService)
             try
             {
                 // Initialize the .NET client of NotifyNL API service
-                var notifyClient = new NotificationClient(
+                var notifyClient = new NotificationClient(    // TODO: Client to be resolved by IClientFactory (to be testable)
                     this._configuration.Notify.API.BaseUrl(),
                     this._configuration.User.API.Key.NotifyNL());
 
-                // Determine first possible Email template ID if nothing was provided
-                List<TemplateResponse>? allTemplates = (await notifyClient.GetAllTemplatesAsync("email")).templates; // NOTE: Assign to variables for debug purposes
-                emailTemplateId ??= allTemplates.First().id;
+                // Determine template type
+                string templateType = s_templateTypes[notifyMethod];
 
+                // Determine first possible Email template ID if nothing was provided
+                List<TemplateResponse>? allTemplates = (await notifyClient.GetAllTemplatesAsync(templateType)).templates; // NOTE: Assign to variables for debug purposes
+                templateId ??= allTemplates.First().id;
+
+                // TODO: To be extracted into a dedicated service
                 // NOTE: Empty personalization
                 if (personalization.Count == 1 || personalization.ContainsKey("key"))
                 {
-                    _ = await notifyClient.SendEmailAsync(emailAddress, emailTemplateId);
+                    NotificationResponse _ = notifyMethod switch
+                    {
+                        NotifyMethods.Email => await notifyClient.SendEmailAsync(contactDetails, templateId),
+                        NotifyMethods.Sms   => await notifyClient.SendSmsAsync(contactDetails, templateId),
+                        _                   => NotImplementedNotifyMethod()
+                    };
                 }
                 // NOTE: Personalization was provided by the user
                 else
                 {
-                    _ = await notifyClient.SendEmailAsync(emailAddress, emailTemplateId, personalization);
+                    NotificationResponse _ = notifyMethod switch
+                    {
+                        NotifyMethods.Email => await notifyClient.SendEmailAsync(contactDetails, templateId, personalization),
+                        NotifyMethods.Sms   => await notifyClient.SendSmsAsync(contactDetails, templateId, personalization),
+                        _                   => NotImplementedNotifyMethod()
+                    };
                 }
 
                 return Ok(new ProcessingFailed.Simplified(HttpStatusCode.Accepted, "Email was successfully send to NotifyNL"));
@@ -161,5 +239,9 @@ namespace EventsHandler.Controllers
                 return BadRequest(new ProcessingFailed.Simplified(HttpStatusCode.InternalServerError, exception.Message));
             }
         }
+
+        private static NotificationResponse NotImplementedNotifyMethod()
+            => throw new ArgumentException(@"This notification method is not supported");
+        #endregion
     }
 }
