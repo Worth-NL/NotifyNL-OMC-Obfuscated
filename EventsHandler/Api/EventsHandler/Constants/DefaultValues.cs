449ba1a177efbc820e481c43e5237d1cf33b7a01
Thomas M. Krystyan <neo.dark.lord@gmail.com>
2024-03-14 17:36:55 +0100

    Upgrade version of the API

--- EventsHandler/Api/EventsHandler/Constants/DefaultValues.cs
+++ EventsHandler/Api/EventsHandler/Constants/DefaultValues.cs
@@ -12,7 +12,7 @@ namespace EventsHandler.Constants
         {
             internal const string Route = "[controller]";
 
-            internal const string Version = "1.6";
+            internal const string Version = "1.61";
         }
         #endregion
 
