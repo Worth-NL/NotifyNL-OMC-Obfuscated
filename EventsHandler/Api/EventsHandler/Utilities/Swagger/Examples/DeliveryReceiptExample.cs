64b1e8daf13c81d4b0bddc202d76d5ad3d4e0952
Thomas M. Krystyan <neo.dark.lord@gmail.com>
2024-03-22 16:56:47 +0100

    Add example for delivery receipt to be displayed in Swagger UI

---
+++ EventsHandler/Api/EventsHandler/Utilities/Swagger/Examples/DeliveryReceiptExample.cs
@@ -0,0 +1,37 @@
+﻿// © 2024, Worth Systems.
+
+using EventsHandler.Behaviors.Mapping.Enums.NotifyNL;
+using EventsHandler.Behaviors.Mapping.Models.POCOs.NotifyNL;
+using Swashbuckle.AspNetCore.Filters;
+using System.Diagnostics.CodeAnalysis;
+
+namespace EventsHandler.Utilities.Swagger.Examples
+{
+    /// <summary>
+    /// An example of delivery status received from "NotifyNL" API web service.
+    /// </summary>
+    /// <seealso cref="IExamplesProvider{T}"/>
+    [ExcludeFromCodeCoverage]
+    internal sealed class DeliveryReceiptExample : IExamplesProvider<DeliveryReceipt>
+    {
+        /// <inheritdoc cref="IExamplesProvider{TModel}.GetExamples"/>
+        public DeliveryReceipt GetExamples()
+        {
+            DateTime currentTime = DateTime.UtcNow;
+
+            return new DeliveryReceipt
+            {
+                Id = Guid.NewGuid(),
+                Reference = "12345678",
+                Recipient = "hello@gov.nl",
+                Status = DeliveryStatus.Delivered,
+                CreatedAt = currentTime,
+                CompletedAt = currentTime.Add(new TimeSpan(seconds: 1, hours: 0, minutes: 0)),
+                SentAt = currentTime.Add(new TimeSpan(seconds: 2, hours: 0, minutes: 0)),
+                Type = NotificationTypes.Email,
+                TemplateId = Guid.NewGuid(),
+                TemplateVersion = 1
+            };
+        }
+    }
+}
