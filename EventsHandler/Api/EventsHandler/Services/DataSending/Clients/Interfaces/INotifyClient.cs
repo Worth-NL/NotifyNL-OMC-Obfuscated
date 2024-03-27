49dda191ae8c3752e86611af7b2ec7c6a9f5a223
Thomas M. Krystyan <neo.dark.lord@gmail.com>
2024-03-27 17:13:48 +0100

    Cleanup

--- EventsHandler/Api/EventsHandler/Services/DataSending/Clients/Interfaces/INotifyClient.cs
+++ EventsHandler/Api/EventsHandler/Services/DataSending/Clients/Interfaces/INotifyClient.cs
@@ -12,15 +12,15 @@ namespace EventsHandler.Services.DataSending.Clients.Interfaces
         /// </summary>
         /// <param name="mobileNumber">The mobile (phone) number.</param>
         /// <param name="templateId">The template identifier.</param>
-        /// <param name="personalisation">The personalisation.</param>
-        internal Task<bool> SendSmsAsync(string mobileNumber, string templateId, Dictionary<string, object> personalisation);
+        /// <param name="personalization">The personalization.</param>
+        internal Task<bool> SendSmsAsync(string mobileNumber, string templateId, Dictionary<string, object> personalization);
 
         /// <summary>
         /// Sends the e-mail asynchronously.
         /// </summary>
         /// <param name="emailAddress">The email address.</param>
         /// <param name="templateId">The template identifier.</param>
-        /// <param name="personalisation">The personalisation.</param>
-        internal Task<bool> SendEmailAsync(string emailAddress, string templateId, Dictionary<string, object> personalisation);
+        /// <param name="personalization">The personalization.</param>
+        internal Task<bool> SendEmailAsync(string emailAddress, string templateId, Dictionary<string, object> personalization);
     }
 }
