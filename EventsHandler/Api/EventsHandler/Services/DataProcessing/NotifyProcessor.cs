7c1023a055b2e8015a55e35553c18358865e3ef7
Thomas M. Krystyan <neo.dark.lord@gmail.com>
2024-04-15 13:22:22 +0200

    Update models and tests

--- EventsHandler/Api/EventsHandler/Services/DataProcessing/NotifyProcessor.cs
+++ EventsHandler/Api/EventsHandler/Services/DataProcessing/NotifyProcessor.cs
@@ -115,8 +115,8 @@ namespace EventsHandler.Services.DataProcessing
                        Channel: Channels.Unknown,
                        Resource: ResourcesEnum.Unknown
                    } &&
-                   string.Equals(notification.MainObject?.AbsoluteUri, testUrl) &&
-                   string.Equals(notification.ResourceUrl?.AbsoluteUri, testUrl);
+                   string.Equals(notification.MainObject.AbsoluteUri, testUrl) &&
+                   string.Equals(notification.ResourceUrl.AbsoluteUri, testUrl);
         }
     }
 }
