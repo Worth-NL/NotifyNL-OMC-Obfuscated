877d809b269b74d511333fe6020b977b7c7b3f82
Thomas M. Krystyan <neo.dark.lord@gmail.com>
2024-05-16 16:12:46 +0200

    Simplify tests

--- EventsHandler/Tests/IntegrationTests/EventsHandler.IntegrationTests/Services/DataQuerying/DataQueryServiceTests.cs
+++ EventsHandler/Tests/IntegrationTests/EventsHandler.IntegrationTests/Services/DataQuerying/DataQueryServiceTests.cs
@@ -1,8 +1,6 @@
 ﻿// © 2023, Worth Systems.
 
 using EventsHandler.Behaviors.Mapping.Models.POCOs.NotificatieApi;
-using EventsHandler.Behaviors.Mapping.Models.POCOs.OpenKlant;
-using EventsHandler.Behaviors.Mapping.Models.POCOs.OpenZaak;
 using EventsHandler.Configuration;
 using EventsHandler.Services.DataLoading.Strategy.Interfaces;
 using EventsHandler.Services.DataQuerying;
@@ -16,7 +14,6 @@ using EventsHandler.Utilities._TestHelpers;
 using Microsoft.Extensions.Configuration;
 using SecretsManager.Services.Authentication.Encryptions.Strategy;
 using SecretsManager.Services.Authentication.Encryptions.Strategy.Context;
-using System.Text.Json;
 
 namespace EventsHandler.IntegrationTests.Services.DataQuerying
 {
@@ -47,53 +44,6 @@ namespace EventsHandler.IntegrationTests.Services.DataQuerying
             this._notification = NotificationEventHandler.GetNotification_Test_WithOrphans_ManuallyCreated();
         }
 
-        [Test, Ignore("Used for debug purposes only")]  // TODO: To be replaced by mocks later
-        public async Task GetCaseAsync_ReturnsExpectedResult()
-        {
-            // Act
-            string result = (await this._dataQuery!.From(this._notification).GetCaseAsync()).Name;
-
-            // Assert
-            Assert.That(result, Is.EqualTo("Klacht afhandeling"));
-        }
-
-        [Test, Ignore("Used for debug purposes only")]  // TODO: To be replaced by mocks later
-        public async Task GetCitizenDetailsAsync_ReturnsExpectedResult()
-        {
-            // Act
-            CitizenDetails result = await this._dataQuery!.From(this._notification).GetCitizenDetailsAsync();
-
-            // Assert
-            string serializedResult = JsonSerializer.Serialize(result).Replace("\\", "").Replace("u002B", "+");  // Replace "\+" with "+"
-
-            Assert.That(serializedResult, Is.EqualTo("""{"count":1,"results":[{"voornaam":"Berthold","voorvoegselAchternaam":"van der","achternaam":"Brecht","geslachtsaanduiding":"","aanmaakkanaal":"-","telefoonnummer":"+31615551461","emailadres":"fmolenaar@worth.systems"}]}"""));
-        }
-
-        [Test, Ignore("Used for debug purposes only")]  // TODO: To be replaced by mocks later
-        public async Task GetCaseStatusesAsync_ReturnsExpectedResult()
-        {
-            // Act
-            CaseStatuses result = await this._dataQuery!.From(this._notification).GetCaseStatusesAsync();
-
-            // Assert
-            string serializedResult = JsonSerializer.Serialize(result);
-
-            Assert.That(serializedResult, Is.EqualTo("""{"count":6,"results":[{"statustype":"https://openzaak.test.denhaag.opengem.nl/catalogi/api/v1/statustypen/529f5ad3-c33e-4200-84e6-c6d6f0c41e88","datumStatusGezet":"2023-09-26T08:15:22Z"},{"statustype":"https://openzaak.test.denhaag.opengem.nl/catalogi/api/v1/statustypen/4c3fcd18-651b-4ada-86af-eb47c72c0654","datumStatusGezet":"2023-09-19T17:02:38Z"},{"statustype":"https://openzaak.test.denhaag.opengem.nl/catalogi/api/v1/statustypen/529f5ad3-c33e-4200-84e6-c6d6f0c41e88","datumStatusGezet":"2023-09-19T17:02:18Z"},{"statustype":"https://openzaak.test.denhaag.opengem.nl/catalogi/api/v1/statustypen/529f5ad3-c33e-4200-84e6-c6d6f0c41e88","datumStatusGezet":"2020-08-24T14:15:22Z"},{"statustype":"https://openzaak.test.denhaag.opengem.nl/catalogi/api/v1/statustypen/529f5ad3-c33e-4200-84e6-c6d6f0c41e88","datumStatusGezet":"2019-08-24T14:15:22Z"},{"statustype":"https://openzaak.test.denhaag.opengem.nl/catalogi/api/v1/statustypen/529f5ad3-c33e-4200-84e6-c6d6f0c41e88","datumStatusGezet":"2018-08-24T14:15:22Z"}]}"""));
-        }
-
-        [Test, Ignore("Used for debug purposes only")]  // TODO: To be replaced by mocks later
-        public async Task GetLastCaseStatusTypeAsync_ReturnsExpectedResult()
-        {
-            // Arrange
-            CaseStatuses caseStatuses = JsonSerializer.Deserialize<CaseStatuses>("""{"count":2,"results":[{"statustype":"https://openzaak.test.denhaag.opengem.nl/catalogi/api/v1/statustypen/529f5ad3-c33e-4200-84e6-c6d6f0c41e88","datumStatusGezet":"2023-09-26T08:15:22Z"},{"statustype":"https://openzaak.test.denhaag.opengem.nl/catalogi/api/v1/statustypen/4c3fcd18-651b-4ada-86af-eb47c72c0654","datumStatusGezet":"2023-09-19T17:02:38Z"}]}""");
-
-            // Act
-            CaseStatusType result = await this._dataQuery!.From(this._notification).GetLastCaseStatusTypeAsync(caseStatuses);
-
-            // Assert
-            string serializedResult = JsonSerializer.Serialize(result);
-
-            Assert.That(serializedResult, Is.EqualTo("""{"omschrijvingGeneriek":"Geregistreerd","isEindstatus":false}"""));
-        }
+        // TODO: Complete tests
     }
 }
