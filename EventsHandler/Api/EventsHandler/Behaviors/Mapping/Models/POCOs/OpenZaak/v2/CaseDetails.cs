ded584a69d42bee9c1cded1843a4ef153e3258da
Thomas M. Krystyan <neo.dark.lord@gmail.com>
2024-05-17 16:29:14 +0200

    Add CaseDetails for v2

--- EventsHandler/Api/EventsHandler/Behaviors/Mapping/Models/POCOs/OpenZaak/v2/CaseDetails.cs
+++ EventsHandler/Api/EventsHandler/Behaviors/Mapping/Models/POCOs/OpenZaak/v2/CaseDetails.cs
@@ -4,20 +4,36 @@ using EventsHandler.Behaviors.Mapping.Models.Interfaces;
 using EventsHandler.Constants;
 using System.Text.Json.Serialization;
 
-namespace EventsHandler.Behaviors.Mapping.Models.POCOs.OpenZaak.v1
+namespace EventsHandler.Behaviors.Mapping.Models.POCOs.OpenZaak.v2
 {
     /// <summary>
-    /// The details of the case retrieved from "OpenZaak" (1.0) Web service.
+    /// The details of the case retrieved from "OpenZaak" (2.0) Web service.
     /// </summary>
     /// <seealso cref="IJsonSerializable"/>
     public struct CaseDetails : IJsonSerializable
     {
         /// <summary>
+        /// Gets the case identification.
+        /// </summary>
+        [JsonInclude]
+        [JsonPropertyName("identificatie")]
+        [JsonPropertyOrder(0)]
+        public string Identification { get; internal set; } = string.Empty;
+
+        /// <summary>
+        /// Gets the case description.
+        /// </summary>
+        [JsonInclude]
+        [JsonPropertyName("omschrijving")]
+        [JsonPropertyOrder(1)]
+        public string Description { get; internal set; } = string.Empty;
+
+        /// <summary>
         /// Gets the case type.
         /// </summary>
         [JsonInclude]
         [JsonPropertyName("zaaktype")]
-        [JsonPropertyOrder(0)]
+        [JsonPropertyOrder(2)]
         public Uri CaseType { get; internal set; } = DefaultValues.Models.EmptyUri;
 
         /// <summary>
