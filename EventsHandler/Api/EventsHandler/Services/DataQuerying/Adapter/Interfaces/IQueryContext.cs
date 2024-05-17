e8a5ab7f223d70f5f4697edae5ddab8526578b74
Thomas M. Krystyan <neo.dark.lord@gmail.com>
2024-05-17 15:00:16 +0200

    Extract OpenZaak into dedicated service

--- EventsHandler/Api/EventsHandler/Services/DataQuerying/Adapter/Interfaces/IQueryContext.cs
+++ EventsHandler/Api/EventsHandler/Services/DataQuerying/Adapter/Interfaces/IQueryContext.cs
@@ -6,20 +6,34 @@ using EventsHandler.Behaviors.Mapping.Models.POCOs.OpenKlant;
 using EventsHandler.Behaviors.Mapping.Models.POCOs.OpenZaak;
 using EventsHandler.Services.DataQuerying.Composition.Interfaces;
 using EventsHandler.Services.DataQuerying.Composition.Strategy.OpenKlant.Interfaces;
+using EventsHandler.Services.DataQuerying.Composition.Strategy.OpenZaak.Interfaces;
 using EventsHandler.Services.DataReceiving.Enums;
 
 namespace EventsHandler.Services.DataQuerying.Adapter.Interfaces
 {
     /// <summary>
-    /// The adapter combining functionalities from other data-querying-related interfaces.
+    /// The adapter combining and adjusting functionalities from other data querying services.
     /// </summary>
+    /// <remarks>
+    ///   This interface is modifying signatures of methods from related services
+    ///   (<see cref="IQueryBase"/>, <see cref="IQueryKlant"/>, <see cref="IQueryZaak"/>)
+    ///   to hide some dependencies inside the <see cref="IQueryContext"/> implementation,
+    ///   make the usage of these methods easier, and base on the injected/setup context.
+    /// </remarks>
+    /// <seealso cref="IQueryBase"/>
+    /// <seealso cref="IQueryKlant"/>
+    /// <seealso cref="IQueryZaak"/>
     internal interface IQueryContext
     {
-        #region IQueryBase        
+        #region IQueryBase
         /// <summary>
         /// Sets the <see cref="NotificationEvent"/> in <see cref="IQueryBase.Notification"/>.
         /// </summary>
         internal void SetNotification(NotificationEvent notification);
+
+        /// <inheritdoc cref="IQueryBase.ProcessGetAsync{TModel}(HttpClientTypes, Uri, string)"/>
+        internal Task<TModel> ProcessGetAsync<TModel>(HttpClientTypes httpsClientType, Uri uri, string fallbackErrorMessage)
+            where TModel : struct, IJsonSerializable;
 
         /// <inheritdoc cref="IQueryBase.ProcessPostAsync{TModel}(HttpClientTypes, Uri, HttpContent, string)"/>
         internal Task<TModel> ProcessPostAsync<TModel>(HttpClientTypes httpsClientType, Uri uri, HttpContent body, string fallbackErrorMessage)
@@ -28,22 +42,29 @@ namespace EventsHandler.Services.DataQuerying.Adapter.Interfaces
 
         #region IQueryKlant
         /// <inheritdoc cref="IQueryKlant.GetCitizenDetailsAsync(IQueryBase, string)"/>
-        internal Task<CitizenDetails> GetCitizenDetailsAsync();
+        /// <remarks>
+        ///   Simpler usage doesn't require providing BSN number first, but it produces an additional
+        ///   overhead since the missing BSN will be queried internally anyway from "OpenZaak" Web service.
+        /// </remarks>
+        internal Task<CitizenDetails> GetCitizenDetailsAsync(string? bsnNumber = null);
         #endregion
 
-        /// <summary>
-        /// Gets the <see cref="Case"/> from "OpenZaak" Web service.
-        /// </summary>
+        #region IQueryZaak
+        /// <inheritdoc cref="IQueryZaak.GetCaseAsync(IQueryBase)"/>
         internal Task<Case> GetCaseAsync();
 
-        /// <summary>
-        /// Gets the status(es) of the specific <see cref="Case"/> from "OpenZaak" Web service.
-        /// </summary>
+        /// <inheritdoc cref="IQueryZaak.GetCaseStatusesAsync(IQueryBase)"/>
         internal Task<CaseStatuses> GetCaseStatusesAsync();
 
-        /// <summary>
-        /// Gets the type of <see cref="CaseStatus"/> from "OpenZaak" Web service.
-        /// </summary>
-        internal Task<CaseStatusType> GetLastCaseStatusTypeAsync(CaseStatuses statuses);
+        /// <inheritdoc cref="IQueryZaak.GetLastCaseStatusTypeAsync(IQueryBase, CaseStatuses)"/>
+        /// <remarks>
+        ///   Simpler usage doesn't require providing <see cref="CaseStatus"/>es, but it produces an additional
+        ///   overhead since the missing statuses will be queried internally anyway from "OpenZaak" Web service.
+        /// </remarks>
+        internal Task<CaseStatusType> GetLastCaseStatusTypeAsync(CaseStatuses? statuses = null);
+
+        /// <inheritdoc cref="IQueryZaak.GetBsnNumberAsync"/>
+        internal Task<string> GetBsnNumberAsync();
+        #endregion
     }
 }
--- EventsHandler/Api/EventsHandler/Services/DataQuerying/Adapter/QueryContext.cs
+++ EventsHandler/Api/EventsHandler/Services/DataQuerying/Adapter/QueryContext.cs
@@ -3,147 +3,80 @@
 using EventsHandler.Behaviors.Mapping.Models.POCOs.NotificatieApi;
 using EventsHandler.Behaviors.Mapping.Models.POCOs.OpenKlant;
 using EventsHandler.Behaviors.Mapping.Models.POCOs.OpenZaak;
-using EventsHandler.Behaviors.Mapping.Models.POCOs.OpenZaak.v1;
-using EventsHandler.Configuration;
 using EventsHandler.Services.DataQuerying.Adapter.Interfaces;
 using EventsHandler.Services.DataQuerying.Composition.Interfaces;
 using EventsHandler.Services.DataQuerying.Composition.Strategy.OpenKlant.Interfaces;
+using EventsHandler.Services.DataQuerying.Composition.Strategy.OpenZaak.Interfaces;
 using EventsHandler.Services.DataReceiving.Enums;
-using Resources = EventsHandler.Properties.Resources;
 
 namespace EventsHandler.Services.DataQuerying.Adapter
 {
     /// <inheritdoc cref="IQueryContext"/>
     internal sealed class QueryContext : IQueryContext
     {
-        private readonly WebApiConfiguration _configuration;
-
         private readonly IQueryBase _queryBase;
         private readonly IQueryKlant _queryKlant;
+        private readonly IQueryZaak _queryZaak;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="QueryContext"/> nested class.
         /// </summary>
-        public QueryContext(WebApiConfiguration configuration, IQueryBase queryBase, IQueryKlant queryKlant)
+        public QueryContext(IQueryBase queryBase, IQueryKlant queryKlant, IQueryZaak queryZaak)
         {
-            this._configuration = configuration;
-
             // Composition
             this._queryBase = queryBase;
             this._queryKlant = queryKlant;
+            this._queryZaak = queryZaak;
         }
 
         #region IQueryBase
         /// <inheritdoc cref="IQueryContext.SetNotification(NotificationEvent)"/>
         void IQueryContext.SetNotification(NotificationEvent notification)
-        {
-            this._queryBase.Notification = notification;
-        }
+            => this._queryBase.Notification = notification;
+
+        /// <inheritdoc cref="IQueryContext.ProcessGetAsync{TModel}(HttpClientTypes, Uri, string)"/>
+        async Task<TModel> IQueryContext.ProcessGetAsync<TModel>(HttpClientTypes httpsClientType, Uri uri, string fallbackErrorMessage)
+            => await this._queryBase.ProcessGetAsync<TModel>(httpsClientType, uri, fallbackErrorMessage);
 
         /// <inheritdoc cref="IQueryContext.ProcessPostAsync{TModel}(HttpClientTypes, Uri, HttpContent, string)"/>
         async Task<TModel> IQueryContext.ProcessPostAsync<TModel>(HttpClientTypes httpsClientType, Uri uri, HttpContent body, string fallbackErrorMessage)
-        {
-            return await this._queryBase.ProcessPostAsync<TModel>(httpsClientType, uri, body, fallbackErrorMessage);
-        }
+            => await this._queryBase.ProcessPostAsync<TModel>(httpsClientType, uri, body, fallbackErrorMessage);
         #endregion
 
         #region IQueryKlant
-        /// <inheritdoc cref="IQueryContext.GetCitizenDetailsAsync()"/>
-        async Task<CitizenDetails> IQueryContext.GetCitizenDetailsAsync()
+        /// <inheritdoc cref="IQueryContext.GetCitizenDetailsAsync(string?)"/>
+        async Task<CitizenDetails> IQueryContext.GetCitizenDetailsAsync(string? bsnNumber)
         {
-            // 1. Fetch BSN using "OpenZaak"
-            string bsnNumber = await GetBsnNumberAsync();
+            // 1. Fetch BSN using "OpenZaak" Web service (if it wasn't provided already)
+            bsnNumber ??= await ((IQueryContext)this).GetBsnNumberAsync();
 
-            // 2. Fetch Citizen Details using "OpenKlant"
+            // 2. Fetch citizen details using "OpenKlant" Web service
             return await this._queryKlant.GetCitizenDetailsAsync(this._queryBase, bsnNumber);
         }
         #endregion
 
-
-
-
-
-        #region Internal query methods
+        #region IQueryZaak
         /// <inheritdoc cref="IQueryContext.GetCaseAsync()"/>
-        /// <exception cref="InvalidOperationException"/>
-        /// <exception cref="HttpRequestException"/>
         async Task<Case> IQueryContext.GetCaseAsync()
-        {
-            return await this._queryBase.ProcessGetAsync<Case>(HttpClientTypes.Data, await GetCaseTypeAsync(), Resources.HttpRequest_ERROR_NoCase);
-        }
-        
+            => await this._queryZaak.GetCaseAsync(this._queryBase);
+
         /// <inheritdoc cref="IQueryContext.GetCaseStatusesAsync()"/>
-        /// <exception cref="InvalidOperationException"/>
-        /// <exception cref="HttpRequestException"/>
         async Task<CaseStatuses> IQueryContext.GetCaseStatusesAsync()
+            => await this._queryZaak.GetCaseStatusesAsync(this._queryBase);
+
+        /// <inheritdoc cref="IQueryContext.GetLastCaseStatusTypeAsync(CaseStatuses?)"/>
+        async Task<CaseStatusType> IQueryContext.GetLastCaseStatusTypeAsync(CaseStatuses? statuses)
         {
-            // Predefined URL components
-            string statusesEndpoint = $"https://{GetSpecificOpenZaakDomain()}/zaken/api/v1/statussen";
+            // 1. Fetch case statuses (if they weren't provided already) from "OpenZaak" Web service
+            statuses ??= await ((IQueryContext)this).GetCaseStatusesAsync();
 
-            // Request URL
-            Uri caseStatuses = new($"{statusesEndpoint}?zaak={this._queryBase.Notification.MainObject}");
-
-            return await this._queryBase.ProcessGetAsync<CaseStatuses>(HttpClientTypes.Data, caseStatuses, Resources.HttpRequest_ERROR_NoCaseStatuses);
-        }
-        
-        /// <inheritdoc cref="IQueryContext.GetLastCaseStatusTypeAsync(CaseStatuses)"/>
-        /// <exception cref="InvalidOperationException"/>
-        /// <exception cref="HttpRequestException"/>
-        async Task<CaseStatusType> IQueryContext.GetLastCaseStatusTypeAsync(CaseStatuses statuses)
-        {
-            // Request URL
-            Uri lastStatusTypeUri = statuses.LastStatus().Type;
-
-            return await this._queryBase.ProcessGetAsync<CaseStatusType>(HttpClientTypes.Data, lastStatusTypeUri, Resources.HttpRequest_ERROR_NoCaseStatusType);
-        }
-        #endregion
-
-        #region Private query methods
-        /// <summary>
-        /// Gets the callback <see cref="Uri"/> to obtain <see cref="Case"/> type from "OpenZaak" Web service.
-        /// </summary>
-        private async Task<Uri> GetCaseTypeAsync()
-        {
-            return this._queryBase.Notification.Attributes.CaseType ?? (await GetCaseDetailsAsync()).CaseType;
+            // 2. Fetch the case status type from the last case status from "OpenZaak" Web service
+            return await this._queryZaak.GetLastCaseStatusTypeAsync(this._queryBase, statuses.Value);
         }
 
-        /// <summary>
-        /// Gets the <see cref="Case"/> details from "OpenZaak" Web service.
-        /// </summary>
-        private async Task<CaseDetails> GetCaseDetailsAsync()
-        {
-            return await this._queryBase.ProcessGetAsync<CaseDetails>(HttpClientTypes.Data, this._queryBase.Notification.MainObject, Resources.HttpRequest_ERROR_NoCaseDetails);
-        }
-
-        /// <summary>
-        /// Gets BSN number of a specific citizen from "OpenZaak" Web service.
-        /// </summary>
-        private async Task<string> GetBsnNumberAsync() => (await GetCaseRoleAsync()).Citizen.BsnNumber;
-
-        /// <summary>
-        /// Gets the <see cref="Case"/> role from "OpenZaak" Web service.
-        /// </summary>
-        private async Task<CaseRoles> GetCaseRoleAsync()
-        {
-            // Predefined URL components
-            string rolesEndpoint = $"https://{GetSpecificOpenZaakDomain()}/zaken/api/v1/rollen";
-            const string roleType = "natuurlijk_persoon";
-
-            // Request URL
-            Uri caseWithRoleUri = new($"{rolesEndpoint}?zaak={this._queryBase.Notification.MainObject}&betrokkeneType={roleType}");
-
-            return await this._queryBase.ProcessGetAsync<CaseRoles>(HttpClientTypes.Data, caseWithRoleUri, Resources.HttpRequest_ERROR_NoCaseRole);
-        }
-        #endregion
-
-        #region Helper methods
-        /// <summary>
-        /// Gets the domain part of the organization-specific (e.g., municipality) "OpenZaak" Web service URI.
-        /// <para>
-        ///   <code>http(s)://[DOMAIN]/ApiEndpoint</code>
-        /// </para>
-        /// </summary>
-        private string GetSpecificOpenZaakDomain() => this._configuration.User.Domain.OpenZaak();
+        /// <inheritdoc cref="IQueryContext.GetBsnNumberAsync()"/>
+        async Task<string> IQueryContext.GetBsnNumberAsync()
+            => await this._queryZaak.GetBsnNumberAsync(this._queryBase);
         #endregion
     }
 }
