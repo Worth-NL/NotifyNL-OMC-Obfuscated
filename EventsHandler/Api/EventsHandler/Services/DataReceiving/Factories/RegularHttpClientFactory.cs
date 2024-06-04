ef973f1a89530f68e8e158f72a048714f9f1f47c
Thomas M. Krystyan <neo.dark.lord@gmail.com>
2024-06-04 10:42:36 +0200

    Add new Network settings to appsettings.json

--- EventsHandler/Api/EventsHandler/Services/DataReceiving/Factories/RegularHttpClientFactory.cs
+++ EventsHandler/Api/EventsHandler/Services/DataReceiving/Factories/RegularHttpClientFactory.cs
@@ -1,5 +1,6 @@
 ﻿// © 2023, Worth Systems.
 
+using EventsHandler.Configuration;
 using EventsHandler.Constants;
 using EventsHandler.Services.DataReceiving.Factories.Interfaces;
 using System.Net.Http.Headers;
@@ -9,10 +10,25 @@ namespace EventsHandler.Services.DataReceiving.Factories
     /// <inheritdoc cref="IHttpClientFactory{THttpClient,TParameter}"/>
     internal sealed class RegularHttpClientFactory : IHttpClientFactory<HttpClient, (string Name, string Value)[]>
     {
+        private readonly WebApiConfiguration _configuration;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RegularHttpClientFactory"/> class.
+        /// </summary>
+        public RegularHttpClientFactory(WebApiConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
+        
         /// <inheritdoc cref="IHttpClientFactory{THttpClient,TParameters}.GetHttpClient(TParameters)"/>
         HttpClient IHttpClientFactory<HttpClient, (string Name, string Value)[]>.GetHttpClient((string Name, string Value)[] requestHeaders)
         {
-            var httpClient = new HttpClient();
+            var handler = new SocketsHttpHandler
+            {
+                PooledConnectionLifetime = TimeSpan.FromSeconds(this._configuration.AppSettings.Network.ConnectionLifetimeInSeconds())
+            };
+
+            var httpClient = new HttpClient(handler);
 
             // Set universal Request Headers
             httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(DefaultValues.Request.ContentType));
