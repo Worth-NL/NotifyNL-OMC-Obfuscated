fded3deb984d251347a49142a507f78a8093751f
Thomas M. Krystyan <neo.dark.lord@gmail.com>
2024-06-05 15:37:37 +0200

    Add complex unit test corner cases covering most of the business cases for PartyResult logic

--- EventsHandler/Tests/UnitTests/EventsHandler.Tests/Behaviors/Mapping/Models/POCOs/OpenKlant/v2/PartyResultsTests.cs
+++ EventsHandler/Tests/UnitTests/EventsHandler.Tests/Behaviors/Mapping/Models/POCOs/OpenKlant/v2/PartyResultsTests.cs
@@ -37,79 +37,269 @@ namespace EventsHandler.UnitTests.Behaviors.Mapping.Models.POCOs.OpenKlant.v2
         }
 
         [Test]
-        public void Party_Method_ForExistingResults_WithMatchingPreferredAddress_ReturnsExpectedResult()
+        public void Party_Method_ForExistingResults_With_MatchingPreferredAddress_ReturnsExpectedResult_Email()
+        {
+            // Arrange
+            var testId = Guid.NewGuid();
+
+            WebApiConfiguration testConfiguration = ConfigurationHandler.GetValidAppSettingsConfiguration();
+            PartyResult testPartyEmail = GetTestPartyResult_Email(testConfiguration, testId, testId);
+            PartyResult testPartyPhone = GetTestPartyResult_Phone(testConfiguration, testId, testId);
+            PartyResults testPartyResults = GetTestPartyResults(testPartyEmail, testPartyPhone);
+
+            // Act
+            (PartyResult actualParty, DistributionChannels actualDistChannel, string actualEmailAddress, string actualPhoneNumber)
+                = testPartyResults.Party(testConfiguration);
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(actualParty, Is.EqualTo(testPartyEmail));
+                Assert.That(actualDistChannel, Is.EqualTo(DistributionChannels.Email));
+                Assert.That(actualEmailAddress, Is.EqualTo($"first_{TestEmail}"));
+                Assert.That(actualPhoneNumber, Is.Empty);  // Since email is priority, phone should be ignored
+            });
+        }
+
+        [Test]
+        public void Party_Method_ForExistingResults_Without_MatchingPreferredAddress_ReturnsExpectedResult_Email_FirstEncountered_PriorityOverPhone()
         {
             // Arrange
             WebApiConfiguration testConfiguration = ConfigurationHandler.GetValidAppSettingsConfiguration();
-            
-            var testGuid = Guid.NewGuid();
+            PartyResult testPartyEmail = GetTestPartyResult_Email(testConfiguration, Guid.Empty, Guid.NewGuid());
+            PartyResult testPartyPhone = GetTestPartyResult_Phone(testConfiguration, Guid.Empty, Guid.NewGuid());
+            PartyResults testPartyResults = GetTestPartyResults(testPartyEmail, testPartyPhone);
 
-            const string testName = "John";
-            const string testPrefix = "";
-            const string testSurname = "Doe";
-            const string testAddressValue = "john.does@gmail.com";
+            // Act
+            (PartyResult actualParty, DistributionChannels actualDistChannel, string actualEmailAddress, string actualPhoneNumber)
+                = testPartyResults.Party(testConfiguration);
 
-            string testAddressType = testConfiguration.AppSettings.Variables.EmailGenericDescription();
-            
-            var testParty = new PartyResult
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(actualParty, Is.EqualTo(testPartyEmail));
+                Assert.That(actualDistChannel, Is.EqualTo(DistributionChannels.Email));
+                Assert.That(actualEmailAddress, Is.EqualTo($"first_{TestEmail}"));  // First encountered email is returned because the preferred address couldn't be determined
+                Assert.That(actualPhoneNumber, Is.Empty);  // Since email is priority, phone should be ignored
+            });
+        }
+        
+        [Test]
+        public void Party_Method_ForExistingResults_With_MatchingPreferredAddress_ReturnsExpectedResult_Phone()
+        {
+            // Arrange
+            var testId = Guid.NewGuid();
+
+            WebApiConfiguration testConfiguration = ConfigurationHandler.GetValidAppSettingsConfiguration();
+            PartyResult testParty = GetTestPartyResult_Phone(testConfiguration, testId, testId);
+            PartyResults testPartyResults = GetTestPartyResults(testParty);
+
+            // Act
+            (PartyResult actualParty, DistributionChannels actualDistChannel, string actualEmailAddress, string actualPhoneNumber)
+                = testPartyResults.Party(testConfiguration);
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(actualParty, Is.EqualTo(testParty));
+                Assert.That(actualDistChannel, Is.EqualTo(DistributionChannels.Sms));
+                Assert.That(actualEmailAddress, Is.Empty);  // Since email is priority, phone should be ignored
+                Assert.That(actualPhoneNumber, Is.EqualTo($"first_{TestPhone}"));
+            });
+        }
+
+        [Test]
+        public void Party_Method_ForExistingResults_Without_MatchingPreferredAddress_ReturnsExpectedResult_Phone_FirstEncountered()
+        {
+            // Arrange
+            WebApiConfiguration testConfiguration = ConfigurationHandler.GetValidAppSettingsConfiguration();
+            PartyResult testParty = GetTestPartyResult_Phone(testConfiguration, Guid.Empty, Guid.NewGuid());
+            PartyResults testPartyResults = GetTestPartyResults(testParty);
+
+            // Act
+            (PartyResult actualParty, DistributionChannels actualDistChannel, string actualEmailAddress, string actualPhoneNumber)
+                = testPartyResults.Party(testConfiguration);
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(actualParty, Is.EqualTo(testParty));
+                Assert.That(actualDistChannel, Is.EqualTo(DistributionChannels.Sms));
+                Assert.That(actualEmailAddress, Is.Empty);  // Since email is priority, phone should be ignored
+                Assert.That(actualPhoneNumber, Is.EqualTo($"first_{TestPhone}"));
+            });
+        }
+
+        #region Helper methods
+        private const string TestEmail = "john.doe@gmail.com";
+        private const string TestPhone = "+44 7911 123456";
+        
+        /// <summary>
+        /// In this set of test data there are phone numbers and e-mails combined, so e-mail should be always prioritized.
+        /// </summary>
+        private static PartyResult GetTestPartyResult_Email(WebApiConfiguration configuration, Guid partyId, Guid addressId)
+        {
+            return new PartyResult
             {
                 PreferredDigitalAddress = new DigitalAddressShort
                 {
-                    Id = testGuid
+                    Id = partyId
                 },
                 Identification = new PartyIdentification
                 {
                     Details = new PartyDetails
                     {
-                        Name = testName,
-                        SurnamePrefix = testPrefix,
-                        Surname = testSurname
+                        Name = "John",
+                        SurnamePrefix = string.Empty,
+                        Surname = "Doe"
                     }
                 },
                 Expansion = new Expansion
                 {
                     DigitalAddresses = new List<DigitalAddressLong>
                     {
-                        new(), // Just empty address
-                        new()
+                        new(), // Just empty address (might be not fully initialized)
+
+                        new()  // Preferred address and valid type but empty e-mail (cannot be used)
                         {
-                            Id = testGuid,
-                            Value = testAddressValue,
-                            Type = testAddressType
+                            Id = partyId,
+                            Value = string.Empty,
+                            Type = configuration.AppSettings.Variables.EmailGenericDescription()
+                        },
+
+                        new()  // Preferred address and valid email but empty type (cannot be used)
+                        {
+                            Id = partyId,
+                            Value = $"emptyType_{TestEmail}",
+                            Type = string.Empty
+                        },
+
+                        new()  // Preferred address but unsupported type (only e-mail and phone numbers)
+                        {
+                            Id = partyId,
+                            Value = "https://www.facebook.com/john.doe",
+                            Type = "Facebook"
+                        },
+
+                        new()  // Never preferred address but matching 1st phone (e-mails have priority)
+                        {
+                            Id = GetUniqueId(addressId),
+                            Value = TestPhone,
+                            Type = configuration.AppSettings.Variables.PhoneGenericDescription()
+                        },
+
+                        new()  // Can be preferred address or not (addressId) matching 1st e-mail
+                        {
+                            Id = addressId,
+                            Value = $"first_{TestEmail}",
+                            Type = configuration.AppSettings.Variables.EmailGenericDescription()
+                        },
+
+                        new()  // Never preferred address but matching 2nd e-mail (not first encountered)
+                        {
+                            Id = GetUniqueId(addressId),
+                            Value = $"second_{TestEmail}",
+                            Type = configuration.AppSettings.Variables.EmailGenericDescription()
                         }
                     }
                 }
             };
 
-            PartyResults partyResults = GetTestPartyResults(testParty);
-
-            // Act
-            (PartyResult actualParty, DistributionChannels actualDistChannel,
-             string actualEmailAddress, string actualPhoneNumber) =
-                partyResults.Party(testConfiguration);
-
-            // Assert
-            Assert.Multiple(() =>
+            static Guid GetUniqueId(Guid addressId)
             {
-                Assert.That(actualParty, Is.EqualTo(testParty));
-                Assert.That(actualDistChannel, Is.EqualTo(DistributionChannels.Email));
-                Assert.That(actualEmailAddress, Is.EqualTo(testAddressValue));
-                Assert.That(actualPhoneNumber, Is.Empty);
-            });
+                var newId = Guid.NewGuid();
+
+                return newId != addressId  // NOTE: In turbo rare cases newly generated GUID can be identical
+                    ? newId
+                    : GetUniqueId(addressId);  // Retry
+            }
         }
 
-        #region Helper methods
-        private static PartyResults GetTestPartyResults(params PartyResult[] roles)
+        /// <summary>
+        /// In this set of test data there is no emails, so phone numbers will always be used.
+        /// </summary>
+        private static PartyResult GetTestPartyResult_Phone(WebApiConfiguration configuration, Guid partyId, Guid addressId)
+        {
+            return new PartyResult
+            {
+                PreferredDigitalAddress = new DigitalAddressShort
+                {
+                    Id = partyId
+                },
+                Identification = new PartyIdentification
+                {
+                    Details = new PartyDetails
+                    {
+                        Name = "Jane",
+                        SurnamePrefix = string.Empty,
+                        Surname = "Doe"
+                    }
+                },
+                Expansion = new Expansion
+                {
+                    DigitalAddresses = new List<DigitalAddressLong>
+                    {
+                        new(), // Just empty address (might be not fully initialized)
+
+                        new()  // Preferred address and valid type but empty phone (cannot be used)
+                        {
+                            Id = partyId,
+                            Value = string.Empty,
+                            Type = configuration.AppSettings.Variables.PhoneGenericDescription()
+                        },
+
+                        new()  // Preferred address and valid phone but empty type (cannot be used)
+                        {
+                            Id = partyId,
+                            Value = $"emptyType_{TestPhone}",
+                            Type = string.Empty
+                        },
+
+                        new()  // Preferred address but unsupported type (only e-mail and phone numbers)
+                        {
+                            Id = partyId,
+                            Value = "https://www.facebook.com/john.doe",
+                            Type = "Facebook"
+                        },
+
+                        new()  // Can be preferred address or not (addressId) matching 1st phone
+                        {
+                            Id = addressId,
+                            Value = $"first_{TestPhone}",
+                            Type = configuration.AppSettings.Variables.PhoneGenericDescription()
+                        },
+
+                        new()  // Never preferred address but matching 2nd phone (not first encountered)
+                        {
+                            Id = GetUniqueId(addressId),
+                            Value = $"second_{TestPhone}",
+                            Type = configuration.AppSettings.Variables.PhoneGenericDescription()
+                        }
+                    }
+                }
+            };
+
+            static Guid GetUniqueId(Guid addressId)
+            {
+                var newId = Guid.NewGuid();
+
+                return newId != addressId  // NOTE: In turbo rare cases newly generated GUID can be identical
+                    ? newId
+                    : GetUniqueId(addressId);  // Retry
+            }
+        }
+
+        private static PartyResults GetTestPartyResults(params PartyResult[] parties)
         {
             var partyResults = new PartyResults
             {
                 Results = new List<PartyResult>
                 {
-                    new()  // Just empty result to always have at least a few of them
+                    new()  // Just empty result to always have at least a few of them + to handle uninitialized ones
                 }
             };
             
-            partyResults.Results.AddRange(roles);
+            partyResults.Results.AddRange(parties);
 
             return partyResults;
         }
