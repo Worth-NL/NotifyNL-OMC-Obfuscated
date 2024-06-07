93d08f8cc4638ec9b217c8fa48ca6db650916489
Thomas M. Krystyan <neo.dark.lord@gmail.com>
2024-06-07 11:45:56 +0200

    Init ContactRegistration for 2nd workflow

---
+++ EventsHandler/Api/EventsHandler/Services/Telemetry/v2/ContactRegistration.cs
@@ -0,0 +1,18 @@
+﻿// © 2024, Worth Systems.
+
+using EventsHandler.Behaviors.Communication.Enums;
+using EventsHandler.Behaviors.Mapping.Models.POCOs.NotificatieApi;
+using EventsHandler.Services.Telemetry.Interfaces;
+
+namespace EventsHandler.Services.Telemetry.v2
+{
+    /// <inheritdoc cref="ITelemetryService"/>
+    /// <seealso cref="ITelemetryService"/>
+    internal sealed class ContactRegistration : ITelemetryService
+    {
+        public Task<string> ReportCompletionAsync(NotificationEvent notification, NotifyMethods notificationMethod, string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
