ca6bbd46fe4397e9e2c0c3c59d5785cf69d9cb22
Thomas M. Krystyan <neo.dark.lord@gmail.com>
2024-06-07 14:05:40 +0200

    Remove tests checking Telemetry service used by NotifySender

--- EventsHandler/Tests/IntegrationTests/EventsHandler.IntegrationTests/Services/DataSending/NotifySenderTests.cs
+++ EventsHandler/Tests/IntegrationTests/EventsHandler.IntegrationTests/Services/DataSending/NotifySenderTests.cs
@@ -1,6 +1,5 @@
 ﻿// © 2023, Worth Systems.
 
-using EventsHandler.Behaviors.Communication.Enums;
 using EventsHandler.Behaviors.Communication.Strategy.Models.DTOs;
 using EventsHandler.Behaviors.Mapping.Models.POCOs.NotificatieApi;
 using EventsHandler.Services.DataReceiving.Factories.Interfaces;
@@ -8,7 +7,6 @@ using EventsHandler.Services.DataSending;
 using EventsHandler.Services.DataSending.Clients.Interfaces;
 using EventsHandler.Services.DataSending.Interfaces;
 using EventsHandler.Services.Serialization.Interfaces;
-using EventsHandler.Services.Telemetry.Interfaces;
 
 #pragma warning disable IDE0008 // Use explicit type
 
@@ -89,43 +87,6 @@ namespace EventsHandler.IntegrationTests.Services.DataSending
                     It.IsAny<Dictionary<string, object>>(),
                     It.IsAny<string>()), Times.Once);
         }
-        
-        [Test]
-        public async Task SendSmsAsync_Calls_IFeedbackServiceMethod_ReportCompletionAsync()
-        {
-            // Arrange
-            Mock<ITelemetryService> mockedTelemetry = GetMockedTelemetry();
-            this._testNotifySender = GetTestSendingService(mockedTelemetry: mockedTelemetry);
-            NotificationEvent testNotification = GetTestNotification();
-
-            // Act
-            await this._testNotifySender.SendSmsAsync(testNotification, new NotifyData());
-
-            // Assert
-            mockedTelemetry.Verify(mock =>
-                mock.ReportCompletionAsync(
-                    It.IsAny<NotificationEvent>(),
-                    It.IsAny<NotifyMethods>(),
-                    It.IsAny<string>()), Times.Once);
-        }
-
-        [Test]
-        public async Task SendEmailAsync_Calls_IFeedbackServiceMethod_ReportCompletionAsync()
-        {
-            // Arrange
-            Mock<ITelemetryService> mockedTelemetry = GetMockedTelemetry();
-            this._testNotifySender = GetTestSendingService(mockedTelemetry: mockedTelemetry);
-            NotificationEvent testNotification = GetTestNotification();
-
-            // Act
-            await this._testNotifySender.SendEmailAsync(testNotification, new NotifyData());
-
-            // Assert
-            mockedTelemetry.Verify(mock => mock.ReportCompletionAsync(
-                It.IsAny<NotificationEvent>(),
-                It.IsAny<NotifyMethods>(),
-                It.IsAny<string>()), Times.Once);
-        }
 
         [Test]
         public async Task HttpClient_IsCached_AsExpected()
@@ -133,14 +94,13 @@ namespace EventsHandler.IntegrationTests.Services.DataSending
             #region First phase of the test (HttpClient will be just created)
             // Arrange
             Mock<ISerializationService> mockedSerializer = GetMockedSerializer();
-            Mock<ITelemetryService> mockedTelemetry = GetMockedTelemetry();
             Mock<INotifyClient> firstMockedNotifyClient = GetMockedNotifyClient();
             var firstMockedClientFactory = new Mock<IHttpClientFactory<INotifyClient, string>>(MockBehavior.Strict);
             firstMockedClientFactory
                 .Setup(mock => mock.GetHttpClient(It.IsAny<string>()))
                 .Returns(firstMockedNotifyClient.Object);
 
-            this._testNotifySender = new NotifySender(firstMockedClientFactory.Object, mockedSerializer.Object, mockedTelemetry.Object);
+            this._testNotifySender = new NotifySender(firstMockedClientFactory.Object, mockedSerializer.Object);
 
             NotificationEvent testNotification = GetTestNotification();
             
@@ -163,7 +123,7 @@ namespace EventsHandler.IntegrationTests.Services.DataSending
                 .Setup(mock => mock.GetHttpClient(It.IsAny<string>()))
                 .Returns(secondMockedNotifyClient.Object);
 
-            this._testNotifySender = new NotifySender(secondMockedClientFactory.Object, mockedSerializer.Object, mockedTelemetry.Object);
+            this._testNotifySender = new NotifySender(secondMockedClientFactory.Object, mockedSerializer.Object);
             
             // Act
             await this._testNotifySender.SendEmailAsync(testNotification, new NotifyData());
@@ -186,8 +146,7 @@ namespace EventsHandler.IntegrationTests.Services.DataSending
 
         #region Helper methods
         private static ISendingService<NotificationEvent, NotifyData> GetTestSendingService(
-            Mock<INotifyClient>? mockedClient = null,
-            Mock<ITelemetryService>? mockedTelemetry = null)
+            Mock<INotifyClient>? mockedClient = null)
         {
             var mockedClientFactory = new Mock<IHttpClientFactory<INotifyClient, string>>(MockBehavior.Strict);
             mockedClientFactory.Setup(mock => mock.GetHttpClient(It.IsAny<string>()))
@@ -195,9 +154,7 @@ namespace EventsHandler.IntegrationTests.Services.DataSending
 
             Mock<ISerializationService> mockedSerializer = GetMockedSerializer();
 
-            mockedTelemetry ??= GetMockedTelemetry();
-
-            return new NotifySender(mockedClientFactory.Object, mockedSerializer.Object, mockedTelemetry.Object);
+            return new NotifySender(mockedClientFactory.Object, mockedSerializer.Object);
         }
 
         private static Mock<INotifyClient> GetMockedNotifyClient()
@@ -234,18 +191,6 @@ namespace EventsHandler.IntegrationTests.Services.DataSending
                 .Returns(new NotificationEvent());
 
             return serializerMock;
-        }
-
-        private static Mock<ITelemetryService> GetMockedTelemetry()
-        {
-            var mockedTelemetry = new Mock<ITelemetryService>(MockBehavior.Strict);
-            mockedTelemetry.Setup(mock => mock.ReportCompletionAsync(
-                    It.IsAny<NotificationEvent>(),
-                    It.IsAny<NotifyMethods>(),
-                    It.IsAny<string>()))
-                .ReturnsAsync(string.Empty);
-
-            return mockedTelemetry;
         }
 
         private static NotificationEvent GetTestNotification() => new()
