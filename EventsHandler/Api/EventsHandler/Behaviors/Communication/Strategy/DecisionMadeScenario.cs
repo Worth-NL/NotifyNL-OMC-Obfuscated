38be7094368607107ba7b99398bc081e67904e9f
Thomas M. Krystyan <neo.dark.lord@gmail.com>
2024-06-18 14:08:57 +0200

    Add new scenario

---
+++ EventsHandler/Api/EventsHandler/Behaviors/Communication/Strategy/DecisionMadeScenario.cs
@@ -0,0 +1,58 @@
+﻿// © 2024, Worth Systems.
+
+using EventsHandler.Behaviors.Communication.Strategy.Base;
+using EventsHandler.Behaviors.Communication.Strategy.Interfaces;
+using EventsHandler.Behaviors.Mapping.Models.POCOs.NotificatieApi;
+using EventsHandler.Behaviors.Mapping.Models.POCOs.OpenKlant;
+using EventsHandler.Behaviors.Mapping.Models.POCOs.OpenZaak;
+using EventsHandler.Configuration;
+using EventsHandler.Services.DataQuerying.Interfaces;
+
+namespace EventsHandler.Behaviors.Communication.Strategy
+{
+    /// <summary>
+    /// <inheritdoc cref="INotifyScenario"/>
+    /// The strategy for "Decision made" scenario.
+    /// </summary>
+    /// <seealso cref="BaseScenario"/>
+    internal sealed class DecisionMadeScenario : BaseScenario
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DecisionMadeScenario"/> class.
+        /// </summary>
+        public DecisionMadeScenario(WebApiConfiguration configuration, IDataQueryService<NotificationEvent> dataQuery)
+            : base(configuration, dataQuery)
+        {
+        }
+
+        #region Polymorphic
+        /// <inheritdoc cref="BaseScenario.GetSmsTemplateId()"/>
+        protected override string GetSmsTemplateId()
+          => this.Configuration.User.TemplateIds.Sms.DecisionMade();
+
+        /// <inheritdoc cref="BaseScenario.GetSmsPersonalization(Case, CommonPartyData)"/>
+        protected override Dictionary<string, object> GetSmsPersonalization(Case @case, CommonPartyData partyData)
+        {
+            return new Dictionary<string, object>
+            {
+                { "zaak.omschrijving", @case.Name },
+                { "zaak.identificatie", @case.Identification }
+            };
+        }
+
+        /// <inheritdoc cref="BaseScenario.GetEmailTemplateId()"/>
+        protected override string GetEmailTemplateId()
+          => this.Configuration.User.TemplateIds.Email.DecisionMade();
+
+        /// <inheritdoc cref="BaseScenario.GetEmailPersonalization(Case, CommonPartyData)"/>
+        protected override Dictionary<string, object> GetEmailPersonalization(Case @case, CommonPartyData partyData)
+        {
+            return new Dictionary<string, object>
+            {
+                { "zaak.omschrijving", @case.Name },
+                { "zaak.identificatie", @case.Identification }
+            };
+        }
+        #endregion
+    }
+}
