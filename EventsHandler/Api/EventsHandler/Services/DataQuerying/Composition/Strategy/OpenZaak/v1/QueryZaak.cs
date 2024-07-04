940d82e26d40459a8cc912fb85f3b09cd43aa3a3
Thomas M. Krystyan <neo.dark.lord@gmail.com>
2024-07-04 15:54:17 +0200

    Update summaries

--- EventsHandler/Api/EventsHandler/Services/DataQuerying/Composition/Strategy/OpenZaak/v1/QueryZaak.cs
+++ EventsHandler/Api/EventsHandler/Services/DataQuerying/Composition/Strategy/OpenZaak/v1/QueryZaak.cs
@@ -14,7 +14,7 @@ namespace EventsHandler.Services.DataQuerying.Composition.Strategy.OpenZaak.v1
 {
     /// <inheritdoc cref="IQueryZaak"/>
     /// <remarks>
-    ///   Version: "OpenZaak" (1.0) Web API service | "OMC workflow" v1.
+    ///   Version: "OpenZaak" (v1+) Web API service | "OMC workflow" v1.
     /// </remarks>
     /// <seealso cref="IVersionDetails"/>
     internal sealed class QueryZaak : IQueryZaak
