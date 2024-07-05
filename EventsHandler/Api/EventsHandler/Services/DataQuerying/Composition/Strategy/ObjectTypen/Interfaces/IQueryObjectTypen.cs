3c04dea603f4ba26a586aa6f44371336b8ca55c1
Thomas M. Krystyan <neo.dark.lord@gmail.com>
2024-07-05 14:14:21 +0200

    Introduce new IQueryObjectTypen service

---
+++ EventsHandler/Api/EventsHandler/Services/DataQuerying/Composition/Strategy/ObjectTypen/Interfaces/IQueryObjectTypen.cs
@@ -0,0 +1,25 @@
+﻿// © 2024, Worth Systems.
+
+using EventsHandler.Behaviors.Versioning;
+using EventsHandler.Configuration;
+
+namespace EventsHandler.Services.DataQuerying.Composition.Strategy.ObjectTypen.Interfaces
+{
+    /// <summary>
+    /// The methods querying specific data from "ObjectTypen" Web API service.
+    /// </summary>
+    /// <seealso cref="IVersionDetails"/>
+    internal interface IQueryObjectTypen : IVersionDetails, IDomain
+    {
+        /// <inheritdoc cref="WebApiConfiguration"/>
+        protected internal WebApiConfiguration Configuration { get; set; }
+
+        /// <inheritdoc cref="IVersionDetails.Name"/>
+        string IVersionDetails.Name => "ObjectTypen";
+
+        #region Polymorphic (Domain)
+        /// <inheritdoc cref="IDomain.GetDomain"/>
+        string IDomain.GetDomain() => this.Configuration.User.Domain.ObjectTypen();
+        #endregion
+    }
+}
