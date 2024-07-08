c95464551ced7302d7f9e8d9f14360e7de9e0b91
Thomas M. Krystyan <neo.dark.lord@gmail.com>
2024-07-08 15:57:36 +0200

    Add extension method extracting GUID from given URI + unit tests

---
+++ EventsHandler/Tests/UnitTests/EventsHandler.Tests/Extensions/UriExtensionsTests.cs
@@ -0,0 +1,54 @@
+﻿// © 2024, Worth Systems.
+
+using EventsHandler.Constants;
+using EventsHandler.Extensions;
+
+namespace EventsHandler.UnitTests.Extensions
+{
+    [TestFixture]
+    public sealed class UriExtensionsTests
+    {
+        [Test]
+        public void GetGuid_ForMissingUri_ReturnsEmptyGuid()
+        {
+            // Act
+            Guid actualGuid = UriExtensions.GetGuid(null);
+
+            // Assert
+            Assert.That(actualGuid, Is.Default);
+        }
+        
+        [Test]
+        public void GetGuid_ForDefaultUri_ReturnsEmptyGuid()
+        {
+            // Act
+            Guid actualGuid = DefaultValues.Models.EmptyUri.GetGuid();
+
+            // Assert
+            Assert.That(actualGuid, Is.Default);
+        }
+
+        [Test]
+        public void GetGuid_ForInvalidUri_ReturnsEmptyGuid()
+        {
+            // Act
+            Guid actualGuid = new Uri("https://www.google.com/").GetGuid();
+
+            // Assert
+            Assert.That(actualGuid, Is.Default);
+        }
+        
+        [Test]
+        public void GetGuid_ForValidUri_ReturnsExtractedGuid()
+        {
+            // Arrange
+            var expectedGuid = new Guid("12345678-1234-1234-1234-123456789012");
+            
+            // Act
+            Guid actualGuid = new Uri($"https://www.google.com/{expectedGuid}").GetGuid();
+
+            // Assert
+            Assert.That(actualGuid, Is.EqualTo(expectedGuid));
+        }
+    }
+}
