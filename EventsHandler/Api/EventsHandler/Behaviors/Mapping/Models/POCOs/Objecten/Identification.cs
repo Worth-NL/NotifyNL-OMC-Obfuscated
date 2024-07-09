7ab4ce58688deb3f273b42dbaaec72cdb4367503
Thomas M. Krystyan <neo.dark.lord@gmail.com>
2024-07-09 13:11:41 +0200

    Prepare identification and identification type

---
+++ EventsHandler/Api/EventsHandler/Behaviors/Mapping/Models/POCOs/Objecten/Identification.cs
@@ -0,0 +1,23 @@
+﻿// © 2024, Worth Systems.
+
+using EventsHandler.Behaviors.Mapping.Enums.Objecten;
+using EventsHandler.Behaviors.Mapping.Models.Interfaces;
+using System.Text.Json.Serialization;
+
+namespace EventsHandler.Behaviors.Mapping.Models.POCOs.Objecten
+{
+    /// <summary>
+    /// The identification related to <see cref="Data"/> retrieved from "Objecten" Web API service.
+    /// </summary>
+    /// <seealso cref="IJsonSerializable"/>
+    public struct Identification : IJsonSerializable
+    {
+        /// <summary>
+        /// Gets the type of the <see cref="Identification"/>.
+        /// </summary>
+        [JsonInclude]
+        [JsonPropertyName("type")]
+        [JsonPropertyOrder(0)]
+        public IdTypes Type { get; internal set; }
+    }
+}
