e1d5b788e44eead8b276312e10f589dc50b33f5a
Thomas M. Krystyan <neo.dark.lord@gmail.com>
2024-07-11 17:19:37 +0200

    New client type enum for ObjectTypen

--- EventsHandler/Api/EventsHandler/Services/DataReceiving/Enums/HttpClientTypes.cs
+++ EventsHandler/Api/EventsHandler/Services/DataReceiving/Enums/HttpClientTypes.cs
@@ -14,7 +14,7 @@ namespace EventsHandler.Services.DataReceiving.Enums
         /// </summary>
         Unknown = 0,
 
-        #region OpenZaak
+        #region OpenZaak (values: 1-10)
         /// <summary>
         /// The <see cref="HttpClient"/> used to obtain data from "OpenZaak" v1+ Web API service.
         /// </summary>
@@ -24,7 +24,7 @@ namespace EventsHandler.Services.DataReceiving.Enums
         OpenZaak_v1 = 1,
         #endregion
 
-        #region OpenKlant
+        #region OpenKlant (values: 11-20)
         /// <summary>
         /// The <see cref="HttpClient"/> used to obtain data from "OpenKlant" v1+ Web API service.
         /// </summary>
@@ -42,17 +42,27 @@ namespace EventsHandler.Services.DataReceiving.Enums
         OpenKlant_v2 = 12,
         #endregion
 
-        #region Objecten
+        #region Objecten (values: 21-30)
         /// <summary>
         /// The <see cref="HttpClient"/> used to obtain data from "Objecten" v2+ Web API service.
         /// </summary>
         /// <remarks>
         /// Authorization: Static API key.
         /// </remarks>
-        Objecten = 22,
+        Objecten = 21,
         #endregion
 
-        #region Telemetry
+        #region Objecten (values: 31-40)
+        /// <summary>
+        /// The <see cref="HttpClient"/> used to obtain data from "ObjectTypen" v2+ Web API service.
+        /// </summary>
+        /// <remarks>
+        /// Authorization: Static API key.
+        /// </remarks>
+        ObjectTypen = 31,
+        #endregion
+
+        #region Telemetry (values: 41-50)
         // NOTE: "OpenZaak" v1+ is part of the "OMC workflow" v1, but those version numbers
         //       are not synonymous. For example, "OMC workflow" v2 includes "OpenKlant" v2+
         //       but also "OpenZaak" v1+. For more details check "OMC - Documentation.md".
