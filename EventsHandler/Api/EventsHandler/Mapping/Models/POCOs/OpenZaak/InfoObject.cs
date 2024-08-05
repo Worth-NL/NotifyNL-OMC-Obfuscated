8878949e97ac0b131f71fb06fc3f66c1c118df0e
Thomas M. Krystyan <neo.dark.lord@gmail.com>
2024-08-05 16:52:19 +0200

    Add new InfoObject model

---
+++ EventsHandler/Api/EventsHandler/Mapping/Models/POCOs/OpenZaak/InfoObject.cs
@@ -0,0 +1,35 @@
+﻿// © 2024, Worth Systems.
+
+using EventsHandler.Mapping.Enums.NotificatieApi;
+using EventsHandler.Mapping.Enums.OpenZaak;
+using EventsHandler.Mapping.Models.Interfaces;
+using System.Text.Json.Serialization;
+
+namespace EventsHandler.Mapping.Models.POCOs.OpenZaak
+{
+    /// <summary>
+    /// The information object related to the <see cref="Decision"/> retrieved from "OpenZaak" Web API service.
+    /// </summary>
+    /// <seealso cref="IJsonSerializable" />
+    public struct InfoObject : IJsonSerializable
+    {
+        /// <inheritdoc cref="PrivacyNotices"/>
+        [JsonInclude]
+        [JsonPropertyName("vertrouwelijkheidaanduiding")]
+        [JsonPropertyOrder(0)]
+        public PrivacyNotices Confidentiality { get; internal set; }  // TODO: Test options for this enum. Only "openbaar" is expected and this enum is reused from notification
+
+        /// <inheritdoc cref="MessageStatus"/>
+        [JsonInclude]
+        [JsonPropertyName("status")]
+        [JsonPropertyOrder(1)]
+        public MessageStatus Status { get; internal set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InfoObject"/> struct.
+        /// </summary>
+        public InfoObject()
+        {
+        }
+    }
+}
