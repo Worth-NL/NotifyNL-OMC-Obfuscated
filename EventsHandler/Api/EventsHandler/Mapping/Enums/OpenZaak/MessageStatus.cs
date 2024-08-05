a4e3aa7962b987fa9d6a924fb635d59c18765138
Thomas M. Krystyan <neo.dark.lord@gmail.com>
2024-08-05 16:51:56 +0200

    Add new MessageStatus enum

---
+++ EventsHandler/Api/EventsHandler/Mapping/Enums/OpenZaak/MessageStatus.cs
@@ -0,0 +1,27 @@
+﻿// © 2024, Worth Systems.
+
+using EventsHandler.Constants;
+using EventsHandler.Mapping.Converters;
+using System.Text.Json.Serialization;
+
+namespace EventsHandler.Mapping.Enums.OpenZaak
+{
+    /// <summary>
+    /// Gets the status of the message.
+    /// </summary>
+    [JsonConverter(typeof(SafeJsonStringEnumMemberConverter<MessageStatus>))]
+    public enum MessageStatus
+    {
+        /// <summary>
+        /// Default value.
+        /// </summary>
+        [JsonPropertyName(DefaultValues.Models.DefaultEnumValueName)]
+        Unknown = 0,
+
+        /// <summary>
+        /// Have to be confidential.
+        /// </summary>
+        [JsonPropertyName("definitief")]
+        Definitive = 1
+    }
+}
