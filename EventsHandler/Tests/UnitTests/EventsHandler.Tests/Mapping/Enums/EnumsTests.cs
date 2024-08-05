0596259fd763d315c035e2b145d612e0c382fb3a
Thomas M. Krystyan <neo.dark.lord@gmail.com>
2024-08-05 16:51:56 +0200

    Add new MessageStatus enum

--- EventsHandler/Tests/UnitTests/EventsHandler.Tests/Mapping/Enums/EnumsTests.cs
+++ EventsHandler/Tests/UnitTests/EventsHandler.Tests/Mapping/Enums/EnumsTests.cs
@@ -3,6 +3,7 @@
 using EventsHandler.Mapping.Enums.NotificatieApi;
 using EventsHandler.Mapping.Enums.NotifyNL;
 using EventsHandler.Mapping.Enums.OpenKlant;
+using EventsHandler.Mapping.Enums.OpenZaak;
 using System.Text.Json;
 
 namespace EventsHandler.UnitTests.Mapping.Enums
@@ -11,31 +12,33 @@ namespace EventsHandler.UnitTests.Mapping.Enums
     public sealed class EnumsTests
     {
         #region Serialization
-        // Actions
+        // NotificatieApi: Actions
         [TestCase(typeof(Actions), Actions.Create, "create")]
         [TestCase(typeof(Actions), Actions.Update, "update")]
         [TestCase(typeof(Actions), Actions.Destroy, "destroy")]
-        // Channels
+        // NotificatieApi: Channels
         [TestCase(typeof(Channels), Channels.Cases, "zaken")]
         [TestCase(typeof(Channels), Channels.Objects, "objecten")]
         [TestCase(typeof(Channels), Channels.Decisions, "besluiten")]
-        // Privacy notices
+        // NotificatieApi: Privacy notices
         [TestCase(typeof(PrivacyNotices), PrivacyNotices.Confidential, "vertrouwelijk")]
         [TestCase(typeof(PrivacyNotices), PrivacyNotices.NonConfidential, "openbaar")]
-        // Resources
+        // NotificatieApi: Resources
         [TestCase(typeof(Resources), Resources.Case, "zaak")]
         [TestCase(typeof(Resources), Resources.Object, "object")]
         [TestCase(typeof(Resources), Resources.Status, "status")]
         [TestCase(typeof(Resources), Resources.Decision, "besluitinformatieobject")]
-        // Distribution channels
+        // OpenKlant: Distribution channels
         [TestCase(typeof(DistributionChannels), DistributionChannels.None, "geen")]
         [TestCase(typeof(DistributionChannels), DistributionChannels.Email, "email")]
         [TestCase(typeof(DistributionChannels), DistributionChannels.Sms, "sms")]
         [TestCase(typeof(DistributionChannels), DistributionChannels.Both, "beiden")]
-        // Notification types
+        // OpenZaak: MessageStatus
+        [TestCase(typeof(MessageStatus), MessageStatus.Definitive, "definitief")]
+        // NotifyNL: Notification types
         [TestCase(typeof(NotificationTypes), NotificationTypes.Email, "email")]
         [TestCase(typeof(NotificationTypes), NotificationTypes.Sms, "sms")]
-        // Delivery statuses
+        // NotifyNL: Delivery statuses
         [TestCase(typeof(DeliveryStatuses), DeliveryStatuses.Created, "created")]
         [TestCase(typeof(DeliveryStatuses), DeliveryStatuses.Sending, "sending")]
         [TestCase(typeof(DeliveryStatuses), DeliveryStatuses.Delivered, "delivered")]
@@ -62,31 +65,33 @@ namespace EventsHandler.UnitTests.Mapping.Enums
         #endregion
 
         #region Deserialization
-        // Actions
+        // NotificatieApi: Actions
         [TestCase("create", typeof(Actions), Actions.Create)]
         [TestCase("update", typeof(Actions), Actions.Update)]
         [TestCase("destroy", typeof(Actions), Actions.Destroy)]
-        // Channels
+        // NotificatieApi: Channels
         [TestCase("zaken", typeof(Channels), Channels.Cases)]
         [TestCase("objecten", typeof(Channels), Channels.Objects)]
         [TestCase("besluiten", typeof(Channels), Channels.Decisions)]
-        // Privacy notices
+        // NotificatieApi: Privacy notices
         [TestCase("vertrouwelijk", typeof(PrivacyNotices), PrivacyNotices.Confidential)]
         [TestCase("openbaar", typeof(PrivacyNotices), PrivacyNotices.NonConfidential)]
-        // Resources
+        // NotificatieApi: Resources
         [TestCase("zaak", typeof(Resources), Resources.Case)]
         [TestCase("object", typeof(Resources), Resources.Object)]
         [TestCase("status", typeof(Resources), Resources.Status)]
         [TestCase("besluitinformatieobject", typeof(Resources), Resources.Decision)]
-        // Distribution channels
+        // OpenKlant: Distribution channels
         [TestCase("geen", typeof(DistributionChannels), DistributionChannels.None)]
         [TestCase("email", typeof(DistributionChannels), DistributionChannels.Email)]
         [TestCase("sms", typeof(DistributionChannels), DistributionChannels.Sms)]
         [TestCase("beiden", typeof(DistributionChannels), DistributionChannels.Both)]
-        // Notification types
+        // OpenZaak: MessageStatus
+        [TestCase("definitief", typeof(MessageStatus), MessageStatus.Definitive)]
+        // NotifyNL: Notification types
         [TestCase("email", typeof(NotificationTypes), NotificationTypes.Email)]
         [TestCase("sms", typeof(NotificationTypes), NotificationTypes.Sms)]
-        // Delivery statuses
+        // NotifyNL: Delivery statuses
         [TestCase("created", typeof(DeliveryStatuses), DeliveryStatuses.Created)]
         [TestCase("sending", typeof(DeliveryStatuses), DeliveryStatuses.Sending)]
         [TestCase("delivered", typeof(DeliveryStatuses), DeliveryStatuses.Delivered)]
@@ -107,11 +112,16 @@ namespace EventsHandler.UnitTests.Mapping.Enums
             Assert.That(Convert.ToInt32(actualEnumValue), Is.EqualTo(expectedEnumValue));
         }
 
+        // NotificatieApi
         [TestCase("?", typeof(Actions), Actions.Unknown)]
         [TestCase("test", typeof(Channels), Channels.Unknown)]
         [TestCase("", typeof(PrivacyNotices), PrivacyNotices.Unknown)]
         [TestCase("123", typeof(Resources), Resources.Unknown)]
+        // OpenKlant
         [TestCase("$#%", typeof(DistributionChannels), DistributionChannels.Unknown)]
+        // OpenZaak
+        [TestCase("!", typeof(MessageStatus), MessageStatus.Unknown)]
+        // NotifyNL
         [TestCase("-", typeof(NotificationTypes), NotificationTypes.Unknown)]
         [TestCase(" ", typeof(DeliveryStatuses), DeliveryStatuses.Unknown)]
         public void JsonSerializer_CustomEnumSerialization_FromUndefinedOption_ToDefaultEnum(string testJsonValue, Type testEnumType, int expectedEnumValue)
