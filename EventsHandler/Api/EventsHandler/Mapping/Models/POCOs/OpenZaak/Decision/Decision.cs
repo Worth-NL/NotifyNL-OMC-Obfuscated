aa4b9b6740223f8c39dc43bbcf1ebfd8cff5b9ee
Thomas M. Krystyan <neo.dark.lord@gmail.com>
2024-08-13 17:36:01 +0200

    Add identification property to Decision model

--- EventsHandler/Api/EventsHandler/Mapping/Models/POCOs/OpenZaak/Decision/Decision.cs
+++ EventsHandler/Api/EventsHandler/Mapping/Models/POCOs/OpenZaak/Decision/Decision.cs
@@ -13,6 +13,17 @@ namespace EventsHandler.Mapping.Models.POCOs.OpenZaak.Decision
     public struct Decision : IJsonSerializable
     {
         /// <summary>
+        /// The identification of the <see cref="Decision"/> in the following format:
+        /// <code>
+        /// BESLUIT-2019-0000000002
+        /// </code>
+        /// </summary>
+        [JsonInclude]
+        [JsonPropertyName("identificatie")]
+        [JsonPropertyOrder(0)]
+        public string Identification { get; internal set; } = string.Empty;
+
+        /// <summary>
         /// The type of the <see cref="Decision"/> in <see cref="Uri"/> format:
         /// <code>
         /// http(s)://Domain/ApiEndpoint/[UUID]
