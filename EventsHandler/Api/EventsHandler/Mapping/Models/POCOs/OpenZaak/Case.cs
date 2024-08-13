217a6182e729d68ca65088641416da1e3cafab21
Thomas M. Krystyan <neo.dark.lord@gmail.com>
2024-08-13 17:09:44 +0200

    Optimize retrieving citizen data by cutting out 1 redundant query

--- EventsHandler/Api/EventsHandler/Mapping/Models/POCOs/OpenZaak/Case.cs
+++ EventsHandler/Api/EventsHandler/Mapping/Models/POCOs/OpenZaak/Case.cs
@@ -1,5 +1,6 @@
 ﻿// © 2023, Worth Systems.
 
+using EventsHandler.Constants;
 using EventsHandler.Mapping.Models.Interfaces;
 using System.Text.Json.Serialization;
 
@@ -26,6 +27,17 @@ namespace EventsHandler.Mapping.Models.POCOs.OpenZaak
         [JsonPropertyName("omschrijving")]
         [JsonPropertyOrder(1)]
         public string Name { get; internal set; } = string.Empty;
+
+        /// <summary>
+        /// The type of the <see cref="Case"/> in <see cref="Uri"/> format.
+        /// </summary>
+        /// <code>
+        /// http(s)://Domain/ApiEndpoint/[UUID]
+        /// </code>
+        [JsonInclude]
+        [JsonPropertyName("zaaktype")]
+        [JsonPropertyOrder(2)]
+        public Uri CaseTypeUri { get; internal set; } = DefaultValues.Models.EmptyUri;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="Case"/> struct.
