95c058ea2b4e36d179c45588ae951d30c3b99867
Thomas M. Krystyan <neo.dark.lord@gmail.com>
2024-08-15 16:34:25 +0200

    Introduce new response struct to be used by strategies

---
+++ EventsHandler/Api/EventsHandler/Services/DataProcessing/Strategy/Responses/ProcessingResponse.cs
@@ -0,0 +1,25 @@
+﻿// © 2024, Worth Systems.
+
+using EventsHandler.Services.DataProcessing.Strategy.Interfaces;
+
+namespace EventsHandler.Services.DataProcessing.Strategy.Responses
+{
+    /// <summary>
+    /// Contains details of processing response performed by a specific OMC <see cref="INotifyScenario"/>.
+    /// </summary>
+    internal readonly struct ProcessingResponse
+    {
+        /// <summary>
+        /// The status of the <see cref="ProcessingResponse"/>.
+        /// </summary>
+        internal bool IsSuccess { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProcessingResponse"/> struct.
+        /// </summary>
+        public ProcessingResponse(bool isSuccess)
+        {
+            this.IsSuccess = isSuccess;
+        }
+    }
+}
