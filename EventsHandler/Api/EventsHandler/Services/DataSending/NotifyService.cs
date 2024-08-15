a21e2b197977545f0ffa818b3f89289b4c2325d3
Thomas M. Krystyan <neo.dark.lord@gmail.com>
2024-08-15 15:43:37 +0200

    Extract common method

--- EventsHandler/Api/EventsHandler/Services/DataSending/NotifyService.cs
+++ EventsHandler/Api/EventsHandler/Services/DataSending/NotifyService.cs
@@ -30,21 +30,28 @@ namespace EventsHandler.Services.DataSending
             IHttpClientFactory<INotifyClient, string> clientFactory,
             ISerializationService serializer)
         {
-            this._clientFactory = clientFactory;
+            this._notifyClientFactory = notifyClientFactory;
             this._serializer = serializer;
+        }
+
+        /// <inheritdoc cref="INotifyService{TModel, TPackage}.SendEmailAsync(TModel, TPackage)"/>
+        async Task<NotifySendResponse> INotifyService<NotificationEvent, NotifyData>.SendEmailAsync(NotificationEvent notification, NotifyData package)
+        {
+            return await ResolveNotifyClient(notification)
+                .SendEmailAsync(emailAddress:    package.ContactDetails,
+                                templateId:      package.TemplateId.ToString(),
+                                personalization: package.Personalization,
+                                reference:       GetEncodedNotification(notification));
         }
 
         /// <inheritdoc cref="INotifyService{TModel, TPackage}.SendSmsAsync(TModel, TPackage)"/>
         async Task<NotifySendResponse> INotifyService<NotificationEvent, NotifyData>.SendSmsAsync(NotificationEvent notification, NotifyData package)
         {
-            string serializedNotification = this._serializer.Serialize(notification);
-            string encodedNotification = serializedNotification.Base64Encode();
-
             return await ResolveNotifyClient(notification)
                 .SendSmsAsync(mobileNumber:    package.ContactDetails,
                               templateId:      package.TemplateId.ToString(),
                               personalization: package.Personalization,
-                              reference:       encodedNotification);
+                              reference:       GetEncodedNotification(notification));
         }
 
         /// <inheritdoc cref="INotifyService{TModel, TPackage}.SendEmailAsync(TModel, TPackage)"/>
@@ -86,6 +93,18 @@ namespace EventsHandler.Services.DataSending
             }
 
             return s_httpClient;
+        }
+
+        /// <summary>
+        /// Gets encoded version of the serialized notification to make it more compact and harder to read.
+        /// </summary>
+        private string GetEncodedNotification(NotificationEvent notification)
+        {
+            // Serialize object to string
+            string serializedNotification = this._serializer.Serialize(notification);
+
+            // Encode string
+            return serializedNotification.Base64Encode();
         }
         #endregion
     }
