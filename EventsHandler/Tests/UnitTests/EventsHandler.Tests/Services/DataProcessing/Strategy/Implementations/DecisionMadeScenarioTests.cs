8db39306f1e81e2896ae02a28a6a8454c8492b3e
Thomas M. Krystyan <neo.dark.lord@gmail.com>
2024-08-27 15:01:56 +0200

    Rename resource

--- EventsHandler/Tests/UnitTests/EventsHandler.Tests/Services/DataProcessing/Strategy/Implementations/DecisionMadeScenarioTests.cs
+++ EventsHandler/Tests/UnitTests/EventsHandler.Tests/Services/DataProcessing/Strategy/Implementations/DecisionMadeScenarioTests.cs
@@ -165,7 +165,7 @@ namespace EventsHandler.UnitTests.Services.DataProcessing.Strategy.Implementatio
                 AbortedNotifyingException? exception =
                     Assert.ThrowsAsync<AbortedNotifyingException>(() => scenario.TryGetDataAsync(default));
 
-                string expectedErrorMessage = Resources.Processing_ABORT_DoNotSendNotification_CaseIdWhitelisted
+                string expectedErrorMessage = Resources.Processing_ABORT_DoNotSendNotification_CaseIdWhitelist
                     .Replace("{0}", "4")
                     .Replace("{1}", "USER_WHITELIST_DECISIONMADE_IDS");
 
