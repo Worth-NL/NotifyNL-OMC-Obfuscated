d679e9edc9e1bf6807ba6dfd7a59c294e376f14f
Thomas M. Krystyan <neo.dark.lord@gmail.com>
2024-08-27 14:07:26 +0200

    Add new MessageReceived scenario

---
+++ EventsHandler/Api/EventsHandler/Services/DataProcessing/Strategy/Implementations/MessageReceivedScenario.cs
@@ -0,0 +1,87 @@
+﻿// © 2024, Worth Systems.
+
+using EventsHandler.Mapping.Models.POCOs.NotificatieApi;
+using EventsHandler.Mapping.Models.POCOs.OpenKlant;
+using EventsHandler.Services.DataProcessing.Strategy.Base;
+using EventsHandler.Services.DataProcessing.Strategy.Base.Interfaces;
+using EventsHandler.Services.DataProcessing.Strategy.Models.DTOs;
+using EventsHandler.Services.DataQuerying.Interfaces;
+using EventsHandler.Services.DataSending.Interfaces;
+using EventsHandler.Services.Settings.Configuration;
+
+namespace EventsHandler.Services.DataProcessing.Strategy.Implementations
+{
+    /// <summary>
+    /// <inheritdoc cref="INotifyScenario"/>
+    /// The strategy for "Message received" scenario.
+    /// </summary>
+    /// <seealso cref="BaseScenario"/>
+    internal sealed class MessageReceivedScenario : BaseScenario
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MessageReceivedScenario"/> class.
+        /// </summary>
+        public MessageReceivedScenario(
+            WebApiConfiguration configuration,
+            IDataQueryService<NotificationEvent> dataQuery,
+            INotifyService<NotificationEvent, NotifyData> notifyService)
+            : base(configuration, dataQuery, notifyService)
+        {
+        }
+
+        #region Polymorphic (PrepareDataAsync)
+        /// <inheritdoc cref="BaseScenario.PrepareDataAsync(NotificationEvent)"/>
+        protected override async Task<CommonPartyData> PrepareDataAsync(NotificationEvent notification)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region Polymorphic (Email logic: template + personalization)
+        /// <inheritdoc cref="BaseScenario.GetEmailTemplateId()"/>
+        protected override Guid GetEmailTemplateId()
+            => this.Configuration.User.TemplateIds.Email.Message();
+
+        private static readonly object s_padlock = new();
+        private static readonly Dictionary<string, object> s_emailPersonalization = new();  // Cached dictionary no need to be initialized every time
+
+        /// <inheritdoc cref="BaseScenario.GetEmailPersonalization(CommonPartyData)"/>
+        protected override Dictionary<string, object> GetEmailPersonalization(CommonPartyData partyData)
+        {
+            lock (s_padlock)
+            {
+                // TODO: Names of parameters can be taken from models and properties(?)
+
+                return s_emailPersonalization;
+            }
+        }
+        #endregion
+
+        #region Polymorphic (SMS logic: template + personalization)
+        /// <inheritdoc cref="BaseScenario.GetSmsTemplateId()"/>
+        protected override Guid GetSmsTemplateId()
+            => this.Configuration.User.TemplateIds.Sms.Message();
+
+        /// <inheritdoc cref="BaseScenario.GetSmsPersonalization(CommonPartyData)"/>
+        protected override Dictionary<string, object> GetSmsPersonalization(CommonPartyData partyData)
+        {
+            return GetEmailPersonalization(partyData);  // NOTE: Both implementations are identical
+        }
+        #endregion
+
+        #region Polymorphic (GetWhitelistName)
+        private static string? s_environmentVariableName;
+
+        /// <inheritdoc cref="BaseScenario.GetWhitelistName"/>
+        protected override string GetWhitelistName()
+        {
+            lock (s_padlock)
+            {
+                return s_environmentVariableName ??= $"{nameof(this.Configuration.User).ToUpper()}_" +
+                                                     $"{nameof(this.Configuration.User.Whitelist).ToUpper()}_" +
+                                                     $"{nameof(this.Configuration.User.Whitelist.Message_Allowed).ToUpper()}";
+            }
+        }
+        #endregion
+    }
+}
