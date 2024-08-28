3a46681b17fd11373c1a6955e80f169fc3322ac0
Thomas M. Krystyan <neo.dark.lord@gmail.com>
2024-08-28 16:25:23 +0200

    Add test covering message validation to MessageReceivedScenario

--- EventsHandler/Tests/UnitTests/EventsHandler.Tests/Services/DataProcessing/Strategy/Implementations/MessageReceivedScenarioTests.cs
+++ EventsHandler/Tests/UnitTests/EventsHandler.Tests/Services/DataProcessing/Strategy/Implementations/MessageReceivedScenarioTests.cs
@@ -1,7 +1,9 @@
 ﻿// © 2024, Worth Systems.
 
+using EventsHandler.Exceptions;
 using EventsHandler.Mapping.Enums.OpenKlant;
 using EventsHandler.Mapping.Models.POCOs.NotificatieApi;
+using EventsHandler.Properties;
 using EventsHandler.Services.DataProcessing.Strategy.Base.Interfaces;
 using EventsHandler.Services.DataProcessing.Strategy.Implementations;
 using EventsHandler.Services.DataProcessing.Strategy.Models.DTOs;
@@ -21,33 +23,49 @@ namespace EventsHandler.UnitTests.Services.DataProcessing.Strategy.Implementatio
         private readonly Mock<IQueryContext> _mockedQueryContext = new(MockBehavior.Strict);
         private readonly Mock<INotifyService<NotificationEvent, NotifyData>> _mockedNotifyService = new(MockBehavior.Strict);
 
-        private WebApiConfiguration _testConfiguration = null!;
-
-        [OneTimeSetUp]
-        public void TestsInitialize()
-        {
-            this._testConfiguration = ConfigurationHandler.GetWebApiConfigurationWith(ConfigurationHandler.TestLoaderTypes.ValidEnvironment);
-        }
-
         [TearDown]
         public void TestsReset()
         {
             this._mockedDataQuery.Reset();
             this._mockedQueryContext.Reset();
             this._mockedNotifyService.Reset();
+
+            this._getDataVerified = false;
+            this._processDataVerified = false;
         }
 
-        [OneTimeTearDown]
-        public void TestsCleanup()
+        #region Test data
+        private static readonly NotificationEvent s_invalidNotification = new();
+        private static readonly NotificationEvent s_validNotification = new()
         {
-            this._testConfiguration.Dispose();
-        }
+            Attributes = new EventAttributes
+            {
+                ObjectTypeUri = new Uri($"http://www.domain.com/{ConfigurationHandler.TestMessageObjectTypeUuid}")
+            }
+        };
+        #endregion
 
         #region TryGetDataAsync()
         [Test]
         public void TryGetDataAsync_NotAllowedMessages_ThrowsAbortedNotifyingException()
         {
+            // Arrange
+            INotifyScenario scenario = ArrangeMessageScenario_TryGetData(false);
 
+            // Act & Assert
+            Assert.Multiple(() =>
+            {
+                AbortedNotifyingException? exception =
+                    Assert.ThrowsAsync<AbortedNotifyingException>(() => scenario.TryGetDataAsync(s_validNotification));
+
+                string expectedMessage = Resources.Processing_ABORT_DoNotSendNotification_MessagesForbidden
+                    .Replace("{0}", "USER_WHITELIST_MESSAGE_ALLOWED");
+
+                Assert.That(exception?.Message.StartsWith(expectedMessage), Is.True);
+                Assert.That(exception?.Message.EndsWith(Resources.Processing_ABORT), Is.True);
+
+                VerifyGetDataMethodCalls(0, 0, 0, 0);
+            });
         }
         #endregion
 
@@ -55,6 +73,10 @@ namespace EventsHandler.UnitTests.Services.DataProcessing.Strategy.Implementatio
         private INotifyScenario ArrangeMessageScenario_TryGetData(
             bool isMessageAllowed, DistributionChannels testDistributionChannel = DistributionChannels.Email)
         {
+            WebApiConfiguration configuration = isMessageAllowed
+                ? ConfigurationHandler.GetWebApiConfigurationWith(ConfigurationHandler.TestLoaderTypes.ValidEnvironment)
+                : ConfigurationHandler.GetWebApiConfigurationWith(ConfigurationHandler.TestLoaderTypes.InvalidEnvironment);
+
             // IQueryContext
 
 
@@ -64,7 +86,71 @@ namespace EventsHandler.UnitTests.Services.DataProcessing.Strategy.Implementatio
                 .Returns(this._mockedQueryContext.Object);
 
             // Decision Scenario
-            return new MessageReceivedScenario(this._testConfiguration, this._mockedDataQuery.Object, this._mockedNotifyService.Object);
+            return new MessageReceivedScenario(configuration, this._mockedDataQuery.Object, this._mockedNotifyService.Object);
+        }
+        #endregion
+
+        #region Verify
+        private bool _getDataVerified;
+        private bool _processDataVerified;
+
+        private void VerifyGetDataMethodCalls(int fromInvokeCount, int getCaseAsyncInvokeCount,
+            int getCaseTypeInvokeCount, int getPartyDataAsyncInvokeCount)
+        {
+            if (this._getDataVerified)
+            {
+                return;
+            }
+
+            // IDataQueryService
+            this._mockedDataQuery
+                .Verify(mock => mock.From(It.IsAny<NotificationEvent>()),
+                Times.Exactly(fromInvokeCount));
+
+            // IQueryContext
+            //this._mockedQueryContext
+            //    .Verify(mock => mock.GetCaseAsync(It.IsAny<object?>()),
+            //    Times.Exactly(getCaseAsyncInvokeCount));
+            
+            //this._mockedQueryContext  // Dependent queries
+            //    .Verify(mock => mock.GetLastCaseTypeAsync(It.IsAny<CaseStatuses?>()),
+            //    Times.Exactly(getCaseTypeInvokeCount));
+            //this._mockedQueryContext
+            //    .Verify(mock => mock.GetCaseStatusesAsync(It.IsAny<Uri?>()),
+            //    Times.Exactly(getCaseTypeInvokeCount));
+
+            //this._mockedQueryContext
+            //    .Verify(mock => mock.GetPartyDataAsync(It.IsAny<string>()),
+            //    Times.Exactly(getPartyDataAsyncInvokeCount));
+
+            this._getDataVerified = true;
+
+            VerifyProcessDataMethodCalls(0, 0);
+        }
+
+        private void VerifyProcessDataMethodCalls(int sendEmailInvokeCount, int sendSmsInvokeCount)
+        {
+            if (this._processDataVerified)
+            {
+                return;
+            }
+            
+            // INotifyService
+            //this._mockedNotifyService
+            //    .Verify(mock => mock.SendEmailAsync(
+            //        It.IsAny<NotificationEvent>(),
+            //        It.IsAny<NotifyData>()),
+            //    Times.Exactly(sendEmailInvokeCount));
+            
+            //this._mockedNotifyService
+            //    .Verify(mock => mock.SendSmsAsync(
+            //        It.IsAny<NotificationEvent>(),
+            //        It.IsAny<NotifyData>()),
+            //    Times.Exactly(sendSmsInvokeCount));
+
+            this._processDataVerified = true;
+
+            VerifyGetDataMethodCalls(0, 0, 0, 0);
         }
         #endregion
     }
