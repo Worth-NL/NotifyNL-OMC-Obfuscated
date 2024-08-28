5a1ad9676459fc5f1369ee5d25a65f12ae05cc93
Thomas M. Krystyan <neo.dark.lord@gmail.com>
2024-08-28 12:48:53 +0200

    Add test class

---
+++ EventsHandler/Tests/UnitTests/EventsHandler.Tests/Services/DataProcessing/Strategy/Implementations/MessageReceivedScenarioTests.cs
@@ -0,0 +1,43 @@
+﻿// © 2024, Worth Systems.
+
+using EventsHandler.Mapping.Models.POCOs.NotificatieApi;
+using EventsHandler.Services.DataProcessing.Strategy.Models.DTOs;
+using EventsHandler.Services.DataQuerying.Adapter.Interfaces;
+using EventsHandler.Services.DataQuerying.Interfaces;
+using EventsHandler.Services.DataSending.Interfaces;
+using EventsHandler.Services.Settings.Configuration;
+using EventsHandler.Utilities._TestHelpers;
+using Moq;
+
+namespace EventsHandler.UnitTests.Services.DataProcessing.Strategy.Implementations
+{
+    [TestFixture]
+    public sealed class MessageReceivedScenarioTests
+    {
+        private readonly Mock<IDataQueryService<NotificationEvent>> _mockedDataQuery = new(MockBehavior.Strict);
+        private readonly Mock<IQueryContext> _mockedQueryContext = new(MockBehavior.Strict);
+        private readonly Mock<INotifyService<NotificationEvent, NotifyData>> _mockedNotifyService = new(MockBehavior.Strict);
+
+        private WebApiConfiguration _testConfiguration = null!;
+
+        [OneTimeSetUp]
+        public void TestsInitialize()
+        {
+            this._testConfiguration = ConfigurationHandler.GetValidEnvironmentConfiguration();
+        }
+
+        [TearDown]
+        public void TestsReset()
+        {
+            this._mockedDataQuery.Reset();
+            this._mockedQueryContext.Reset();
+            this._mockedNotifyService.Reset();
+        }
+
+        [OneTimeTearDown]
+        public void TestsCleanup()
+        {
+            this._testConfiguration.Dispose();
+        }
+    }
+}
