7b4d919c2961cde380073c4f916199452e214f67
Thomas M. Krystyan <neo.dark.lord@gmail.com>
2024-08-29 14:38:28 +0200

    Cleanup

--- EventsHandler/Api/EventsHandler/Services/DataQuerying/Composition/Strategy/Objecten/Interfaces/IQueryObjecten.cs
+++ EventsHandler/Api/EventsHandler/Services/DataQuerying/Composition/Strategy/Objecten/Interfaces/IQueryObjecten.cs
@@ -23,8 +23,8 @@ namespace EventsHandler.Services.DataQuerying.Composition.Strategy.Objecten.Inte
         /// <inheritdoc cref="IVersionDetails.Name"/>
         string IVersionDetails.Name => "Objecten";
 
+        #pragma warning disable CA1822  // These methods can be marked as static but that would be inconsistent for interfaces
         #region Parent (Task)
-        #pragma warning disable CA1822  // The method can be marked as static but that would be inconsistent for interfaces
         /// <summary>
         /// Gets the <see cref="TaskObject"/> from "Objecten" Web API service.
         /// </summary>
@@ -39,11 +39,9 @@ namespace EventsHandler.Services.DataQuerying.Composition.Strategy.Objecten.Inte
                 uri: queryBase.Notification.MainObjectUri,  // Request URL
                 fallbackErrorMessage: Resources.HttpRequest_ERROR_NoTask);
         }
-        #pragma warning restore CA1822
         #endregion
 
         #region Parent (Message)
-        #pragma warning disable CA1822  // The method can be marked as static but that would be inconsistent for interfaces
         /// <summary>
         /// Gets the <see cref="MessageObject"/> from "Objecten" Web API service.
         /// </summary>
@@ -58,8 +56,8 @@ namespace EventsHandler.Services.DataQuerying.Composition.Strategy.Objecten.Inte
                 uri: queryBase.Notification.MainObjectUri,  // Request URL
                 fallbackErrorMessage: Resources.HttpRequest_ERROR_NoMessage);
         }
-        #pragma warning restore CA1822
         #endregion
+        #pragma warning restore CA1822
 
         #region Polymorphic (Domain)
         /// <inheritdoc cref="IDomain.GetDomain"/>
