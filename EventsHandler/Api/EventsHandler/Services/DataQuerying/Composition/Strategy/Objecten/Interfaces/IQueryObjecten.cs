af775cfe8fe1720b14e3c1c4369f3a76f5521d4c
Thomas M. Krystyan <neo.dark.lord@gmail.com>
2024-08-29 14:48:18 +0200

    Validate MainObject if it contains objects endpoint

--- EventsHandler/Api/EventsHandler/Services/DataQuerying/Composition/Strategy/Objecten/Interfaces/IQueryObjecten.cs
+++ EventsHandler/Api/EventsHandler/Services/DataQuerying/Composition/Strategy/Objecten/Interfaces/IQueryObjecten.cs
@@ -8,6 +8,7 @@ using EventsHandler.Services.Versioning.Interfaces;
 using System.Text.Json;
 using EventsHandler.Mapping.Models.POCOs.Objecten.Message;
 using Resources = EventsHandler.Properties.Resources;
+using EventsHandler.Extensions;
 
 namespace EventsHandler.Services.DataQuerying.Composition.Strategy.Objecten.Interfaces
 {
@@ -29,14 +30,22 @@ namespace EventsHandler.Services.DataQuerying.Composition.Strategy.Objecten.Inte
         /// Gets the <see cref="TaskObject"/> from "Objecten" Web API service.
         /// </summary>
         /// <exception cref="HttpRequestException"/>
-        /// <exception cref="JsonException"/>
+        /// <exception cref="JsonException">
+        ///   This method might fail when deserializing generic JSON response from Objects endpoint to <see cref="TaskObject"/> model.
+        /// </exception>
         internal sealed async Task<TaskObject> GetTaskAsync(IQueryBase queryBase)
         {
-            // TODO: Main Object validation
+            // Request URL
+            Uri taskObjectUri = queryBase.Notification.MainObjectUri;
+
+            if (taskObjectUri.IsNotObject())
+            {
+                throw new ArgumentException(Resources.Operation_ERROR_Internal_NotObjectUri);
+            }
 
             return await queryBase.ProcessGetAsync<TaskObject>(
                 httpClientType: HttpClientTypes.Objecten,
-                uri: queryBase.Notification.MainObjectUri,  // Request URL
+                uri: taskObjectUri,
                 fallbackErrorMessage: Resources.HttpRequest_ERROR_NoTask);
         }
         #endregion
@@ -46,14 +55,22 @@ namespace EventsHandler.Services.DataQuerying.Composition.Strategy.Objecten.Inte
         /// Gets the <see cref="MessageObject"/> from "Objecten" Web API service.
         /// </summary>
         /// <exception cref="HttpRequestException"/>
-        /// <exception cref="JsonException"/>
+        /// <exception cref="JsonException">
+        ///   This method might fail when deserializing generic JSON response from Objects endpoint to <see cref="MessageObject"/> model.
+        /// </exception>
         internal sealed async Task<MessageObject> GetMessageAsync(IQueryBase queryBase)
         {
-            // TODO: Main Object validation
+            // Request URL
+            Uri taskObjectUri = queryBase.Notification.MainObjectUri;
+
+            if (taskObjectUri.IsNotObject())
+            {
+                throw new ArgumentException(Resources.Operation_ERROR_Internal_NotObjectUri);
+            }
 
             return await queryBase.ProcessGetAsync<MessageObject>(
                 httpClientType: HttpClientTypes.Objecten,
-                uri: queryBase.Notification.MainObjectUri,  // Request URL
+                uri: taskObjectUri,
                 fallbackErrorMessage: Resources.HttpRequest_ERROR_NoMessage);
         }
         #endregion
