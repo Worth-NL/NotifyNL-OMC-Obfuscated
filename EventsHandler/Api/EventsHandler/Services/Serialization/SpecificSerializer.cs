0382c5b5fce0eab26503bdb96c1423d36cfb1a62
Thomas M. Krystyan <neo.dark.lord@gmail.com>
2024-09-02 15:40:30 +0200

    Display details which fields are required per model

--- EventsHandler/Api/EventsHandler/Services/Serialization/SpecificSerializer.cs
+++ EventsHandler/Api/EventsHandler/Services/Serialization/SpecificSerializer.cs
@@ -1,8 +1,11 @@
 ﻿// © 2023, Worth Systems.
 
+using EventsHandler.Mapping.Models.Interfaces;
 using EventsHandler.Properties;
 using EventsHandler.Services.Serialization.Interfaces;
+using System.Reflection;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace EventsHandler.Services.Serialization
 {
@@ -21,8 +24,23 @@ namespace EventsHandler.Services.Serialization
                 throw new JsonException(message:
                     $"{Resources.Deserialization_ERROR_CannotDeserialize_Message} | " +
                     $"{Resources.Deserialization_ERROR_CannotDeserialize_Target}: {typeof(TModel).Name} | " +
-                    $"{Resources.Deserialization_ERROR_CannotDeserialize_Value}: {json}");
+                    $"{Resources.Deserialization_ERROR_CannotDeserialize_Value}: {json} | " +
+                    $"{Resources.Deserialization_ERROR_CannotDeserialize_Required}: {GetRequiredMembers<TModel>()}");
             }
+        }
+        
+        /// <summary>
+        /// Gets text representation of this specific <see cref="IJsonSerializable"/> object.
+        /// </summary>
+        private static string GetRequiredMembers<TModel>()
+            where TModel : struct, IJsonSerializable
+        {
+            return string.Join(", ", 
+                typeof(TModel)
+                    .GetProperties(BindingFlags.Instance | BindingFlags.Public)
+                    .Where(property => property.GetCustomAttribute<JsonRequiredAttribute>() != null)
+                    .Select(property => property.GetCustomAttribute<JsonPropertyNameAttribute>()?.Name ?? property.Name)
+                    .ToArray());
         }
 
         /// <inheritdoc cref="ISerializationService.Serialize{TModel}(TModel)"/>
--- EventsHandler/Tests/UnitTests/EventsHandler.Tests/Services/Serialization/SpecificSerializerTests.cs
+++ EventsHandler/Tests/UnitTests/EventsHandler.Tests/Services/Serialization/SpecificSerializerTests.cs
@@ -1,7 +1,7 @@
 ﻿// © 2024, Worth Systems.
 
 using EventsHandler.Constants;
-using EventsHandler.Mapping.Models.Interfaces;
+using EventsHandler.Mapping.Models.POCOs.OpenZaak;
 using EventsHandler.Services.Serialization;
 using EventsHandler.Services.Serialization.Interfaces;
 using System.Text.Json;
@@ -14,15 +14,41 @@ namespace EventsHandler.UnitTests.Services.Serialization
         private ISerializationService? _serializer;
 
         #region Test data
-        private const int TestNumber = 50;
-        private readonly string _testJson = $"{{\"Number\":{TestNumber}}}";
+        private const string Name = "begin";
+        private const string Description = "begin";
+        private const string IsFinalStatus = "false";
+        private const string IsNotificationExpected = "true";
 
-        private readonly struct TestFullModel : IJsonSerializable
-        {
-            public int Number { get; init; }
-        }
+        private const string InputJson =
+            $"{{" +
+              $"\"url\":\"https://openzaak.test.notifynl.nl/catalogi/api/v1/statustypen/e22c1e78-1893-4fd7-a674-3900672859c7\"," +
+              $"\"omschrijving\":\"{Name}\"," +
+              $"\"omschrijvingGeneriek\":\"{Description}\"," +
+              $"\"statustekst\":\"begin status\"," +
+              $"\"zaaktype\":\"https://openzaak.test.notifynl.nl/catalogi/api/v1/zaaktypen/54c6063d-d3ae-47dd-90df-9e00cfa122a2\"," +
+              $"\"zaaktypeIdentificatie\":\"1\"," +
+              $"\"volgnummer\":2," +
+              $"\"isEindstatus\":{IsFinalStatus}," +
+              $"\"informeren\":{IsNotificationExpected}," +
+              $"\"doorlooptijd\":null," +
+              $"\"toelichting\":\"begin status\"," +
+              $"\"checklistitemStatustype\":[]," +
+              $"\"catalogus\":\"https://openzaak.test.notifynl.nl/catalogi/api/v1/catalogussen/34061b3c-cc85-4572-ba27-e286c279fb40\"," +
+              $"\"eigenschappen\":[]," +
+              $"\"zaakobjecttypen\":[]," +
+              $"\"beginGeldigheid\":null," +
+              $"\"eindeGeldigheid\":null," +
+              $"\"beginObject\":null," +
+              $"\"eindeObject\":null" +
+            $"}}";
 
-        private readonly struct TestEmptyModel : IJsonSerializable { }
+        private const string OutputJson =
+            $"{{" +
+              $"\"omschrijving\":\"{Name}\"," +
+              $"\"omschrijvingGeneriek\":\"{Description}\"," +
+              $"\"isEindstatus\":{IsFinalStatus}," +
+              $"\"informeren\":{IsNotificationExpected}" +
+            $"}}";
         #endregion
 
         [SetUp]
@@ -36,17 +62,34 @@ namespace EventsHandler.UnitTests.Services.Serialization
         public void Deserialize_TakesValidJson_AndReturnsDeserializedModel()
         {
             // Act
-            TestFullModel actualResult = this._serializer!.Deserialize<TestFullModel>(_testJson);
+            CaseType actualResult = this._serializer!.Deserialize<CaseType>(InputJson);
 
             // Assert
-            Assert.That(actualResult.Number, Is.EqualTo(TestNumber));
+            Assert.Multiple(() =>
+            {
+                Assert.That(actualResult.Name, Is.EqualTo(Name));
+                Assert.That(actualResult.Description, Is.EqualTo(Description));
+                Assert.That(actualResult.IsFinalStatus, Is.EqualTo(Convert.ToBoolean(IsFinalStatus)));
+                Assert.That(actualResult.IsNotificationExpected, Is.EqualTo(Convert.ToBoolean(IsNotificationExpected)));
+            });
         }
 
         [Test]
         public void Deserialize_TakesEmptyJson_ThrowsJsonException()
         {
             // Act & Assert
-            Assert.Throws<JsonException>(() => this._serializer!.Deserialize<TestFullModel>(new object()));
+            Assert.Multiple(() =>
+            {
+                JsonException? exception = Assert.Throws<JsonException>(() => this._serializer!.Deserialize<CaseType>(DefaultValues.Models.EmptyJson));
+
+                const string expectedMessage =
+                    "The given value cannot be deserialized into dedicated target object | " +
+                    "Target: CaseType | " +
+                    "Value: {} | " +
+                    "Required properties: omschrijving, omschrijvingGeneriek, isEindstatus, informeren";
+
+                Assert.That(exception?.Message, Is.EqualTo(expectedMessage));
+            });
         }
         #endregion
 
@@ -55,23 +98,29 @@ namespace EventsHandler.UnitTests.Services.Serialization
         public void Serialize_TakesValidModel_AndReturnsSerializedJson()
         {
             // Arrange
-            var testModel = new TestFullModel { Number = TestNumber };
+            var testModel = new CaseType
+            {
+                Name = Name,
+                Description = Description,
+                IsFinalStatus = Convert.ToBoolean(IsFinalStatus),
+                IsNotificationExpected = Convert.ToBoolean(IsNotificationExpected)
+            };
 
             // Act
             string actualResult = this._serializer!.Serialize(testModel);
 
             // Assert
-            Assert.That(actualResult, Is.EqualTo(_testJson));
+            Assert.That(actualResult, Is.EqualTo(OutputJson));
         }
 
         [Test]
         public void Serialize_TakesUnknownModel_DoesNotThrowException_AndReturnsDefaultJson()
         {
             // Act
-            string actualResult = this._serializer!.Serialize(default(TestEmptyModel));
+            string actualResult = this._serializer!.Serialize(default(CaseType));
 
             // Assert
-            Assert.That(actualResult, Is.EqualTo(DefaultValues.Models.EmptyJson));
+            Assert.That(actualResult, Is.EqualTo("{\"omschrijving\":null,\"omschrijvingGeneriek\":null,\"isEindstatus\":false,\"informeren\":false}"));
         }
         #endregion
     }
