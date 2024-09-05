973f2dba2ee11b80590458e14c96f8cb220a7345
Thomas M. Krystyan <neo.dark.lord@gmail.com>
2024-09-05 15:26:52 +0200

    Prepare two new custom JSON Converters

---
+++ EventsHandler/Api/EventsHandler/Services/Serialization/Converters/DateTimeJsonConverter.cs
@@ -0,0 +1,30 @@
+﻿// © 2024, Worth Systems.
+
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace EventsHandler.Services.Serialization.Converters
+{
+    /// <summary>
+    /// The custom converter specialized in handling <see langword="string"/> types.
+    /// </summary>
+    /// <seealso cref="JsonConverter{TValue}" />
+    internal sealed class DateTimeJsonConverter : JsonConverter<DateTime>
+    {
+        public override bool HandleNull => true;
+
+        /// <inheritdoc cref="JsonConverter{TValue}.Read(ref Utf8JsonReader, Type, JsonSerializerOptions)"/>
+        public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            return reader.TokenType != JsonTokenType.Null 
+                ? reader.GetDateTime()
+                : DateTime.MinValue;
+        }
+
+        /// <inheritdoc cref="JsonConverter{TValue}.Write(Utf8JsonWriter, TValue, JsonSerializerOptions)"/>
+        public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value);
+        }
+    }
+}
