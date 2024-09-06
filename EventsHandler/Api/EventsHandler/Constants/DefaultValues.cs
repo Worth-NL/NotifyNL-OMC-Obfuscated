027e5ce30ac41189079cd929e81640f66118e327
Thomas M. Krystyan <neo.dark.lord@gmail.com>
2024-09-06 10:05:52 +0200

    Update version of OMC to 1.8.10

--- EventsHandler/Api/EventsHandler/Constants/DefaultValues.cs
+++ EventsHandler/Api/EventsHandler/Constants/DefaultValues.cs
@@ -12,7 +12,7 @@ namespace EventsHandler.Constants
         {
             internal const string Route = "[controller]";
 
-            internal const string Version = "1.88";
+            internal const string Version = "1.810";
         }
         #endregion
 
