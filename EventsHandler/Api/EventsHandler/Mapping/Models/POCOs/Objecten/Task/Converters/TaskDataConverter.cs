d253c8c6fd67548c795b33738c2ec90594002025
Thomas M. Krystyan <neo.dark.lord@gmail.com>
2024-09-16 16:04:10 +0200

    Init converter for CommonTaskData

---
+++ EventsHandler/Api/EventsHandler/Mapping/Models/POCOs/Objecten/Task/Converters/TaskDataConverter.cs
@@ -0,0 +1,30 @@
+﻿// © 2024, Worth Systems.
+
+using EventsHandler.Mapping.Models.POCOs.Objecten.Task.vHague;
+
+namespace EventsHandler.Mapping.Models.POCOs.Objecten.Task.Converters
+{
+    /// <summary>
+    /// Converts task data from different versions of "Objecten" into a unified <see cref="CommonTaskData"/>.
+    /// </summary>
+    internal static class TaskDataConverter
+    {
+        /// <summary>
+        /// Converts <see cref="Data"/> from <see cref="Record"/> from <see cref="TaskObject"/> from "Objecten" Web API service (version used by The Hague).
+        /// </summary>
+        /// <returns>
+        ///   The unified <see cref="CommonTaskData"/> DTO model.
+        /// </returns>
+        internal static CommonTaskData ConvertToUnified(this Data taskDataHague)
+        {
+            return new CommonTaskData
+            {
+                CaseUri = taskDataHague.CaseUri,
+                Title = taskDataHague.Title,
+                Status = taskDataHague.Status,
+                ExpirationDate = taskDataHague.ExpirationDate,
+                Identification = taskDataHague.Identification
+            };
+        }
+    }
+}
