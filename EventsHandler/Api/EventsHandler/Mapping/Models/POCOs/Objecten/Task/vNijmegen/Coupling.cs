46ed0210b0d5fe47ca8d0ed4a8fa65409e442de0
Thomas M. Krystyan <neo.dark.lord@gmail.com>
2024-09-16 16:36:45 +0200

    Add new nested model used by TaskObject

---
+++ EventsHandler/Api/EventsHandler/Mapping/Models/POCOs/Objecten/Task/vNijmegen/Coupling.cs
@@ -0,0 +1,40 @@
+﻿// © 2024, Worth Systems.
+
+using EventsHandler.Mapping.Enums.Objecten.vNijmegen;
+using EventsHandler.Mapping.Models.Interfaces;
+using System.Text.Json.Serialization;
+
+namespace EventsHandler.Mapping.Models.POCOs.Objecten.Task.vNijmegen
+{
+    /// <summary>
+    /// The coupling related to the <see cref="TaskObject"/> retrieved from "Objecten" Web API service.
+    /// </summary>
+    /// <seealso cref="IJsonSerializable"/>
+    public struct Coupling : IJsonSerializable
+    {
+        /// <summary>
+        /// The type of the <see cref="Coupling"/>.
+        /// </summary>
+        [JsonRequired]
+        [JsonInclude]
+        [JsonPropertyName("registratie")]
+        [JsonPropertyOrder(0)]
+        public Registrations Type { get; internal set; }
+
+        /// <summary>
+        /// The identification of the <see cref="Coupling"/>.
+        /// </summary>
+        [JsonRequired]
+        [JsonInclude]
+        [JsonPropertyName("uuid")]
+        [JsonPropertyOrder(1)]
+        public Guid Id { get; internal set; } = Guid.Empty;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Coupling"/> struct.
+        /// </summary>
+        public Coupling()
+        {
+        }
+    }
+}
