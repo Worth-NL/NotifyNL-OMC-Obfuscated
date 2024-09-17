ed4c39bfc4e8470da7af119765ca9191cce57e2c
Thomas M. Krystyan <neo.dark.lord@gmail.com>
2024-09-17 16:48:59 +0200

    Adjust PrepareObjectJsonBody method in IQueryObjectTypen

--- EventsHandler/Api/EventsHandler/Services/DataQuerying/Composition/Strategy/ObjectTypen/Interfaces/IQueryObjectTypen.cs
+++ EventsHandler/Api/EventsHandler/Services/DataQuerying/Composition/Strategy/ObjectTypen/Interfaces/IQueryObjectTypen.cs
@@ -1,5 +1,6 @@
 ﻿// © 2024, Worth Systems.
 
+using EventsHandler.Extensions;
 using EventsHandler.Services.Settings.Configuration;
 using EventsHandler.Services.Versioning.Interfaces;
 
@@ -16,6 +17,30 @@ namespace EventsHandler.Services.DataQuerying.Composition.Strategy.ObjectTypen.I
 
         /// <inheritdoc cref="IVersionDetails.Name"/>
         string IVersionDetails.Name => "ObjectTypen";
+
+        #region Parent (Create message object)
+        /// <summary>
+        /// Prepares an object JSON body that can be used by "Objecten" Web API service.
+        /// </summary>
+        /// <param name="objectTypeId">The object type ID.</param>
+        /// <param name="dataJson">The data JSON.</param>
+        /// <returns>
+        ///   Specific JSON used to define objects from "Objecten" Web API service.
+        /// </returns>
+        internal string PrepareObjectJsonBody(Guid objectTypeId, string dataJson)
+        {
+            return $"{{" +
+                     $"\"type\":\"https://{GetDomain()}/api/v1/objecttypes/{objectTypeId}\"," +
+                     $"\"record\":{{" +
+                       $"\"typeVersion\":\"{this.Configuration.AppSettings.Variables.Objecten.MessageObjectType_Version()}\"," +
+                       $"\"data\":{{" +
+                         $"{dataJson}" +
+                       $"}}," +
+                       $"\"startAt\":\"{DateTime.UtcNow.ConvertToDutchDateString()}\"" +
+                     $"}}" +
+                   $"}}";
+        }
+        #endregion
 
         #region Polymorphic (Domain)
         /// <inheritdoc cref="IDomain.GetDomain"/>
