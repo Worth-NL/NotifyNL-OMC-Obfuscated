1436c1f8cf49ec0ba3e76b7824941ff51dcb2ab0
Thomas M. Krystyan <neo.dark.lord@gmail.com>
2024-09-25 14:49:24 +0200

    Add PreparedData model

---
+++ EventsHandler/Api/EventsHandler/Services/DataProcessing/Strategy/Models/DTOs/PreparedData.cs
@@ -0,0 +1,29 @@
+﻿// © 2024, Worth Systems.
+
+using EventsHandler.Mapping.Models.POCOs.OpenKlant;
+using EventsHandler.Mapping.Models.POCOs.OpenZaak;
+using EventsHandler.Services.DataProcessing.Strategy.Base.Interfaces;
+
+namespace EventsHandler.Services.DataProcessing.Strategy.Models.DTOs
+{
+    /// <summary>
+    /// The package of data retrieved from <see cref="INotifyScenario"/>s.
+    /// </summary>
+    internal readonly struct PreparedData
+    {
+        /// <inheritdoc cref="CommonPartyData"/>
+        internal CommonPartyData Party { get; }
+
+        /// <inheritdoc cref="Case.Uri"/>
+        internal Uri CaseUri { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PreparedData"/> struct.
+        /// </summary>
+        public PreparedData(CommonPartyData party, Uri caseUri)
+        {
+            this.Party = party;
+            this.CaseUri = caseUri;
+        }
+    }
+}
