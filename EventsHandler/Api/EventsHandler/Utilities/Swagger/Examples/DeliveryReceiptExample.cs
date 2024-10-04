60b18c135476b94c3c79102d21bd701338e609c4
Thomas M. Krystyan <neo.dark.lord@gmail.com>
2024-10-04 12:21:22 +0200

    Reuse existing Swagger examples

--- EventsHandler/Api/EventsHandler/Utilities/Swagger/Examples/DeliveryReceiptExample.cs
+++ EventsHandler/Api/EventsHandler/Utilities/Swagger/Examples/DeliveryReceiptExample.cs
@@ -5,6 +5,7 @@ using EventsHandler.Mapping.Enums.NotifyNL;
 using EventsHandler.Mapping.Models.POCOs.NotifyNL;
 using Swashbuckle.AspNetCore.Filters;
 using System.Diagnostics.CodeAnalysis;
+using System.Text.Json;
 
 namespace EventsHandler.Utilities.Swagger.Examples
 {
@@ -15,25 +16,7 @@ namespace EventsHandler.Utilities.Swagger.Examples
     [ExcludeFromCodeCoverage(Justification = "This is example model used by Swagger UI; testing how third-party dependency is dealing with it is unnecessary.")]
     internal sealed class DeliveryReceiptExample : IExamplesProvider<DeliveryReceipt>
     {
-        private const string SerializedNotification =
-            $"{{" +
-              $"\"notification\":{{" +
-              $"  \"actie\":\"create\"," +
-              $"  \"kanaal\":\"zaken\"," +
-              $"  \"resource\":\"status\"," +
-              $"  \"kenmerken\":{{" +
-              $"    \"objectType\":\"http://0.0.0.0:0/\"," +
-              $"    \"zaaktype\":\"https://openzaak.test.denhaag.opengem.nl/catalogi/api/v1/zaaktypen/cf57c196-982d-4e2b-a567-d47794642bd7\"," +
-              $"    \"bronorganisatie\":\"286130270\"," +
-              $"    \"vertrouwelijkheidaanduiding\":\"openbaar\"" +
-              $"  }}," +
-              $"  \"hoofdObject\":\"https://openzaak.test.denhaag.opengem.nl/zaken/api/v1/zaken/4205aec5-9f5b-4abf-b177-c5a9946a77af\"," +
-              $"  \"resourceUrl\":\"https://openzaak.test.denhaag.opengem.nl/zaken/api/v1/statussen/11cbdb9f-1445-4424-bf34-0bf066033e03\"," +
-              $"  \"aanmaakdatum\":\"2023-09-22T11:41:46.052Z\"" +
-              $"}}," +
-              $"\"caseUri\":\"http://0.0.0.0:0/\"," +
-              $"\"partyUri\": \"http://0.0.0.0:0/\"" +
-            $"}}";
+        private static readonly string s_serializedNotification = JsonSerializer.Serialize(new NotifyReferenceExample().GetExamples());
 
         /// <inheritdoc cref="IExamplesProvider{TModel}.GetExamples"/>
         public DeliveryReceipt GetExamples()
@@ -43,7 +26,7 @@ namespace EventsHandler.Utilities.Swagger.Examples
             return new DeliveryReceipt
             {
                 Id = Guid.NewGuid(),
-                Reference = SerializedNotification.Base64Encode(),
+                Reference = s_serializedNotification.Base64Encode(),
                 Recipient = "hello@gov.nl",
                 Status = DeliveryStatuses.Delivered,
                 CreatedAt = currentTime,
